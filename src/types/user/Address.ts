10473b8d1a25273e64087cdab00118888ed12b48
Amir <haghighi251@gmail.com>
2024-03-01 19:39:23 +0100

    feat: add TextBox component

---
+++ src/types/user/Address.ts
@@ -0,0 +1,28 @@
+export enum TitleType {
+  Mister = "MISTER",
+  Miss = "MISS",
+}
+
+export type Address = {
+  id: string;
+  firstName?: string;
+  lastName?: string;
+  title?: TitleType;
+  street: string;
+  streetNumber: string;
+  postalCode: string;
+  city: string;
+  country: string;
+  email?: string;
+  phone?: string;
+  mobile?: string;
+  paymentAddress?: boolean;
+  shipmentAddress?: boolean;
+  skipAddressCheck?: boolean;
+  birthdayDay?: string;
+  birthdayMonth?: string;
+  birthdayYear?: string;
+  addition?: string;
+  parcelShopId?: string;
+  pickingPointName?: string;
+};
