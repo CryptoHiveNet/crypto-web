f6524f271d28d0da56891236948c910c78ec7b3e
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-26 17:18:58 +0330

    Update ESLint and Prettier configurations

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,27 +1,28 @@
-import Link from "next/link";
-import { useTranslation } from "../../utils/i18n";
-import { LanguageSwitcher } from "@/types/components/i18n/LanguageSwitcher";
-import AlertBox from "@/types/components/alert/Alert";
+import Link from 'next/link';
+import { useTranslation } from '../../utils/i18n';
+import { LanguageSwitcher } from '@/types/components/i18n/LanguageSwitcher';
+import AlertBox from '@/types/components/alert/Alert';
 
 export default async function Page({ params: { lng } }: any) {
-  const { t } = await useTranslation(lng);
-  return (
-    <>
-      <AlertBox
-        id="dfsdf"
-        additionalContent={<div>This is additional info.</div>}
-        color="warning"
-      >
-        <span>This is the main alert message.</span>
-      </AlertBox>
+   const { t } = await useTranslation(lng);
+   return (
+      <>
+         <AlertBox
+            id='dfsdf'
+            additionalContent={<div>This is additional info.</div>}
+            color='warning'
+         >
+            <div>
+               <span>This is the main alert message.</span>
+            </div>
+         </AlertBox>
 
-
-      <h1>
-        {t("name")}
-        {t("title")}
-      </h1>
-      <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link>
-      <LanguageSwitcher />
-    </>
-  );
+         <h1>
+            {t('name')}
+            {t('title')}
+         </h1>
+         <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
+         <LanguageSwitcher />
+      </>
+   );
 }
