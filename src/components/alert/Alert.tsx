cea52ef67ed3a082bf1d6068d2ca0c046b7b95d0
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-26 20:04:20 +0330

    Update Alert component tests

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,37 +1,42 @@
-"use client";
-import React, { FC, SVGProps } from "react";
-import { Alert } from "flowbite-react";
-import { HiEye, HiInformationCircle } from "react-icons/hi";
-import { IconType } from "react-icons";
+'use client';
+import React, { FC, SVGProps, useState } from 'react';
+import { Alert as FlowbiteAlert } from 'flowbite-react';
+import { HiEye, HiInformationCircle } from 'react-icons/hi';
 
-export type iAlertBoxProps = {
-  id?: string;
-  additionalContent?: JSX.Element;
-  color?: string; // String for color name
-  alertIcon?: FC<SVGProps<SVGSVGElement>>;
-  children: JSX.Element;
-} & Partial<Omit<HTMLDivElement, "type" | "value">>; // HTMLDivElement is more suitable
+export type iAlertProps = {
+   id?: string;
+   additionalContent?: JSX.Element;
+   color?: string; // String for color name
+   alertIcon?: FC<SVGProps<SVGSVGElement>>;
+   children: JSX.Element;
+} & Partial<Omit<HTMLDivElement, 'type' | 'value'>>; // HTMLDivElement is more suitable
 
-const AlertBox: FC<iAlertBoxProps> = ({
-  id,
-  additionalContent,
-  color = "warning",
-  alertIcon=HiInformationCircle,
-  children
+const Alert: FC<iAlertProps> = ({
+   id,
+   additionalContent,
+   color = 'warning',
+   alertIcon = HiInformationCircle,
+   children,
 }) => {
-  const onDismissFunc = () => console.log("Alert Dismissed!")
-  return (
-    <Alert
-      id={id}
-      additionalContent={additionalContent}
-      color={color}
-      icon={alertIcon}
-      onDismiss={onDismissFunc}
-      rounded
-    >
-      {children}
-    </Alert>
-  );
+   const [showAlert, setShowAlert] = useState(true);
+
+   const onDismissFunc = () => setShowAlert(false);
+   return (
+      <>
+         {showAlert && (
+            <FlowbiteAlert
+               id={id}
+               additionalContent={additionalContent}
+               color={color}
+               icon={alertIcon}
+               onDismiss={onDismissFunc}
+               rounded
+            >
+               {children}
+            </FlowbiteAlert>
+         )}
+      </>
+   );
 };
 
-export default AlertBox;
+export default Alert;
