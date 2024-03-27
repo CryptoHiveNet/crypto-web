48f90600bf339542583882b58bcd3b4aed5d3c2d
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-27 18:45:36 +0330

    feat: Add button component with unit tests

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -4,39 +4,39 @@ import { Alert as FlowbiteAlert } from 'flowbite-react';
 import { HiEye, HiInformationCircle } from 'react-icons/hi';
 
 export type iAlertProps = {
-   id?: string;
-   additionalContent?: JSX.Element;
-   color?: string; // String for color name
-   alertIcon?: FC<SVGProps<SVGSVGElement>>;
-   children: JSX.Element;
-} & Partial<Omit<HTMLDivElement, 'type' | 'value'>>; // HTMLDivElement is more suitable
+  id?: string;
+  additionalContent?: JSX.Element;
+  color?: string; // String for color name
+  alertIcon?: FC<SVGProps<SVGSVGElement>>;
+  children: JSX.Element;
+}
 
-const Alert: FC<iAlertProps> = ({
-   id,
-   additionalContent,
-   color = 'warning',
-   alertIcon = HiInformationCircle,
-   children,
-}) => {
-   const [showAlert, setShowAlert] = useState(true);
+const Alert = ({
+  id,
+  additionalContent,
+  color = 'warning',
+  alertIcon = HiInformationCircle,
+  children,
+}: iAlertProps) => {
+  const [showAlert, setShowAlert] = useState(true);
 
-   const onDismissFunc = () => setShowAlert(false);
-   return (
-      <>
-         {showAlert && (
-            <FlowbiteAlert
-               id={id}
-               additionalContent={additionalContent}
-               color={color}
-               icon={alertIcon}
-               onDismiss={onDismissFunc}
-               rounded
-            >
-               {children}
-            </FlowbiteAlert>
-         )}
-      </>
-   );
+  const onDismissFunc = () => setShowAlert(false);
+  return (
+    <>
+      {showAlert && (
+        <FlowbiteAlert
+          id={id}
+          additionalContent={additionalContent}
+          color={color}
+          icon={alertIcon}
+          onDismiss={onDismissFunc}
+          rounded
+        >
+          {children}
+        </FlowbiteAlert>
+      )}
+    </>
+  );
 };
 
 export default Alert;
