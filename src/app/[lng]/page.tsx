801bc49fd7d255c8cc692e45dd80c18597f72559
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-27 18:45:36 +0330

    feat: Add button component with unit tests

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -2,27 +2,20 @@ import Link from 'next/link';
 import { useTranslation } from '../../utils/i18n';
 import { LanguageSwitcher } from '@/types/components/i18n/LanguageSwitcher';
 import Alert from '@/types/components/alert/Alert';
+import Button from '@/types/components/button/Button';
 
 export default async function Page({ params: { lng } }: any) {
-   const { t } = await useTranslation(lng);
-   return (
-      <>
-         <Alert
-            id='dfsdf'
-            additionalContent={<div>This is additional info.</div>}
-            color='warning'
-         >
-            <div>
-               <span>This is the main alert message.</span>
-            </div>
-         </Alert>
+  const { t } = await useTranslation(lng);
+  return (
+    <>
+      <Button id='test-button'>test button</Button>
 
-         <h1>
-            {t('name')}
-            {t('title')}
-         </h1>
-         <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
-         <LanguageSwitcher />
-      </>
-   );
+      <h1>
+        {t('name')}
+        {t('title')}
+      </h1>
+      <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
+      <LanguageSwitcher />
+    </>
+  );
 }
