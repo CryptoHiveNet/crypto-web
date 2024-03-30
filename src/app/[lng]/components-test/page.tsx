7d45c5e97b8373514f48a380d37b374259344890
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-30 18:54:32 +0330

    fix: update components

--- src/app/[lng]/components-test/page.tsx
+++ src/app/[lng]/components-test/page.tsx
@@ -5,7 +5,9 @@ import ButtonGroup from '@/types/components/buttonGroup/ButtonGroup';
 export default async function Page({ params: { lng } }: any) {
   return (
     <div className='p-10'>
-      <Alert id='test-Alert'>test Alert</Alert>
+      <Alert id='test-Alert' testId='2323'>
+        test Alert
+      </Alert>
       <Button id='test-Button'>test Button</Button>
       <ButtonGroup>
         <Button id='test-Button2'>test Button2</Button>
