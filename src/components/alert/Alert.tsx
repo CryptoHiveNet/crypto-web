cdb64d39905967d3636a60c70eda2b7e8db3de91
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-21 17:56:01 +0330

    fix: added forward ref to the last components

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,91 +1,85 @@
 'use client';
-import React, { FC, SVGProps, useState, useEffect } from 'react';
 import { Alert as FlowbiteAlert } from 'flowbite-react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import { HiInformationCircle } from 'react-icons/hi';
 
-export type iAlertProps = {
-  id?: string;
-  additionalContent?: JSX.Element;
-  role?: string;
-  icon?: FC<SVGProps<SVGSVGElement>>;
-  className?: string;
-  children: JSX.Element | string;
-  testId?: string;
-  onClick?: () => void;
-  onDismiss?: () => void;
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
-  dismissAfter?: number; // Optional time in milliseconds to automatically dismiss the alert
-};
+import { iAlertProps } from '@/types/types/components/components';
 
-const Alert = ({
-  id,
-  additionalContent,
-  role = 'blue',
-  icon = HiInformationCircle,
-  className,
-  children,
-  testId,
-  onClick,
-  onDismiss,
-  onMouseEnter,
-  onMouseLeave,
-  dismissAfter,
-  ...rest
-}: iAlertProps) => {
-  const [showAlert, setShowAlert] = useState(true);
-  const [isMounted, setIsMounted] = useState(true);
-  const transitionDurations = 300;
-  useEffect(() => {
-    let timeoutId: NodeJS.Timeout;
+const Alert = forwardRef<any, iAlertProps>(
+  (
+    {
+      id,
+      additionalContent,
+      role = 'blue',
+      icon = HiInformationCircle,
+      className,
+      children,
+      testId,
+      onClick,
+      onDismiss,
+      onMouseEnter,
+      onMouseLeave,
+      dismissAfter,
+      ...rest
+    }: iAlertProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    const [showAlert, setShowAlert] = useState(true);
+    const [isMounted, setIsMounted] = useState(true);
+    const transitionDurations = 300;
 
-    if (dismissAfter) {
-      timeoutId = setTimeout(() => {
-        setShowAlert(false);
-        if (onDismiss) onDismiss();
-        setTimeout(() => {
-          setIsMounted(false);
-        }, transitionDurations);
-      }, dismissAfter);
-    }
+    useEffect(() => {
+      let timeoutId: NodeJS.Timeout;
 
-    return () => {
-      if (timeoutId) clearTimeout(timeoutId);
+      if (dismissAfter) {
+        timeoutId = setTimeout(() => {
+          setShowAlert(false);
+          if (onDismiss) onDismiss();
+          setTimeout(() => {
+            setIsMounted(false);
+          }, transitionDurations);
+        }, dismissAfter);
+      }
+
+      return () => {
+        if (timeoutId) clearTimeout(timeoutId);
+      };
+    }, [dismissAfter, id, onDismiss]);
+
+    const handleDismiss = () => {
+      setShowAlert(false);
+      if (onDismiss) onDismiss();
+      setTimeout(() => {
+        setIsMounted(false);
+      }, transitionDurations);
     };
-  }, [dismissAfter, id, onDismiss]);
 
-  const handleDismiss = () => {
-    setShowAlert(false);
-    if (onDismiss) onDismiss();
-    setTimeout(() => {
-      setIsMounted(false);
-    }, transitionDurations);
-  };
-
-  return (
-    isMounted && (
-      <div
-        className={`transition-all duration-${transitionDurations} ease ${showAlert ? 'max-h-40 opacity-1' : 'max-h-0 opacity-0'}`}
-      >
-        <FlowbiteAlert
-          id={id}
-          className={className}
-          additionalContent={additionalContent}
-          color={role}
-          icon={icon}
-          rounded
-          data-testid={testId}
-          onClick={onClick}
-          onDismiss={handleDismiss}
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-          {...rest}
+    return (
+      isMounted && (
+        <div
+          className={`transition-all duration-${transitionDurations} ease ${showAlert ? 'max-h-40 opacity-1' : 'max-h-0 opacity-0'}`}
         >
-          {children}
-        </FlowbiteAlert>
-      </div>
-    )
-  );
-};
+          <FlowbiteAlert
+            id={id}
+            className={className}
+            additionalContent={additionalContent}
+            color={role}
+            icon={icon}
+            rounded
+            data-testid={testId}
+            onClick={onClick}
+            onDismiss={handleDismiss}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            ref={ref}
+            {...rest}
+          >
+            {children}
+          </FlowbiteAlert>
+        </div>
+      )
+    );
+  },
+);
 
 export default Alert;
--- src/components/textBox/TextBox.tsx
+++ src/components/textBox/TextBox.tsx
@@ -1,66 +1,56 @@
 'use client';
-import React, { FC, SVGProps } from 'react';
 import { Label, TextInput } from 'flowbite-react';
-import { TextInputType } from '@/types/types/components/textbox';
+import { forwardRef } from 'react';
 
-export type iTextBoxProps = {
-  type: TextInputType;
-  labelText: string;
-  id?: string;
-  className?: string;
-  placeholder?: string;
-  required?: boolean;
-  value?: string;
-  icon?: FC<SVGProps<SVGSVGElement>> | undefined;
-  color?: string;
-  helperText?: string;
-  testId?: string;
-  onClick?: () => void;
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
-};
+import { iTextBoxProps } from '@/types/types/components/components';
 
-const TextBox = ({
-  type,
-  labelText,
-  id,
-  className,
-  placeholder,
-  required,
-  value,
-  icon,
-  color,
-  helperText,
-  testId,
-  onClick,
-  onMouseEnter,
-  onMouseLeave,
-  ...rest
-}: iTextBoxProps) => {
-  return (
-    <div>
-      <div className='mb-2 block'>
-        <Label htmlFor={id} value={labelText} />
+const TextBox = forwardRef<any, iTextBoxProps>(
+  (
+    {
+      type,
+      labelText,
+      id,
+      className,
+      placeholder,
+      required,
+      value,
+      icon,
+      color,
+      helperText,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: iTextBoxProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <div>
+        <div className='mb-2 block'>
+          <Label htmlFor={id} value={labelText} />
+        </div>
+        <TextInput
+          id={id}
+          type={type}
+          className={className}
+          placeholder={placeholder}
+          shadow
+          color={color}
+          helperText={helperText}
+          value={value}
+          icon={icon}
+          required={required}
+          data-testid={testId}
+          onClick={onClick}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+          ref={ref}
+          {...rest}
+        />
       </div>
-      <TextInput
-        id={id}
-        type={type}
-        className={className}
-        placeholder={placeholder}
-        shadow
-        color={color}
-        helperText={helperText}
-        value={value}
-        icon={icon}
-        required={required}
-        data-testid={testId}
-        onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        {...rest}
-      />
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default TextBox;
