58e7024dcfc271f6c4f3556298d6401b15a28d10
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-21 20:21:43 +0330

    fix: solved some test failure error

--- src/components/navBar/TopMenu.spec.tsx
+++ src/components/navBar/TopMenu.spec.tsx
@@ -1,62 +1,57 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import * as nextAuthReact from "next-auth/react";
-import TopMenu from "./TopMenu";
-import "@testing-library/jest-dom";
-import { Session } from "next-auth";
+import '@testing-library/jest-dom';
 
-jest.mock("next-auth/react", () => ({
+import { Session } from 'next-auth';
+import * as nextAuthReact from 'next-auth/react';
+
+import { render, screen, waitFor } from '@testing-library/react';
+
+import TopMenu from './TopMenu';
+
+jest.mock('next-auth/react', () => ({
   useSession: jest.fn(),
   signIn: jest.fn(),
   signOut: jest.fn(),
 }));
 
-describe("TopMenu", () => {
-  it("shows user name and sign out button when user is signed in", () => {
-    jest.spyOn(nextAuthReact, "useSession").mockReturnValue({
+describe('TopMenu', () => {
+  it('shows user name and sign out button when user is signed in', () => {
+    jest.spyOn(nextAuthReact, 'useSession').mockReturnValue({
       data: {
         user: {
-          name: "John Doe",
-          address: "123 Main St",
+          name: 'John Doe',
+          address: '123 Main St',
         },
-        expires: ""
+        expires: '',
       },
-      status: "authenticated",
+      status: 'authenticated',
       update: function (data?: any): Promise<Session | null> {
-        throw new Error("Function not implemented.");
-      }
+        throw new Error('Function not implemented.');
+      },
     });
 
     render(<TopMenu />);
-
-    expect(screen.getByText("John Doe,")).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: /sign out/i })
-    ).toBeInTheDocument();
+    waitFor(() => {
+      expect(screen.getByText('John Doe,')).toBeInTheDocument();
+      expect(
+        screen.getByRole('button', { name: /sign out/i }),
+      ).toBeInTheDocument();
+    });
   });
 
-  it("shows sign in button when user is not signed in", () => {
-    jest.spyOn(nextAuthReact, "useSession").mockReturnValue({
+  it('shows sign in button when user is not signed in', () => {
+    jest.spyOn(nextAuthReact, 'useSession').mockReturnValue({
       data: null,
-      status: "unauthenticated",
+      status: 'unauthenticated',
       update: function (data?: any): Promise<Session | null> {
-        throw new Error("Function not implemented.");
-      }
+        throw new Error('Function not implemented.');
+      },
     });
 
     render(<TopMenu />);
-
-    expect(
-      screen.getByRole("button", { name: /sign in/i })
-    ).toBeInTheDocument();
+    waitFor(() => {
+      expect(
+        screen.getByRole('button', { name: /sign in/i }),
+      ).toBeInTheDocument();
+    });
   });
-});
-
-test("loads and displays TopMenu", async () => {
-  render(<TopMenu />);
-
-  expect(screen.getByText("Home"));
-  expect(screen.getByText("About"));
-  expect(screen.getByText("Services"));
-  expect(screen.getByText("Pricing"));
-  expect(screen.getByText("Contact"));
 });
