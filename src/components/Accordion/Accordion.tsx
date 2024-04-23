2b50a67adc5136438739a66c5ff606ea1a46d1ea
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-23 15:22:13 +0330

    feat: accordion component added with unit tests

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,41 +1,39 @@
+import { Accordion as FlowbiteAccordion } from 'flowbite-react';
 import { forwardRef } from 'react';
 
-import { Accordion as FlowbiteAccordion } from 'flowbite-react';
+import { iAccordionProps } from '@/types/types/components/accordion';
 
-import { AccordionProps } from '@/types/shared/types/components/accordion';
-
-// eslint-disable-next-line react/display-name
-const Accordion = forwardRef<any, AccordionProps>(
-    (
-        {
-            id,
-            collapseAll,
-            className,
-            children,
-            testId,
-            onClick,
-            onMouseEnter,
-            onMouseLeave,
-            ...rest
-        }: AccordionProps,
-        ref: React.Ref<any> | null,
-    ) => {
-        return (
-            <FlowbiteAccordion
-                id={id}
-                collapseAll={collapseAll}
-                className={className}
-                data-testid={testId}
-                onClick={onClick}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                ref={ref}
-                {...rest}
-            >
-                {children}
-            </FlowbiteAccordion>
-        );
-    },
+const Accordion = forwardRef<any, iAccordionProps>(
+  (
+    {
+      id,
+      collapseAll,
+      className,
+      children,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: iAccordionProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <FlowbiteAccordion
+        id={id}
+        collapseAll={collapseAll}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </FlowbiteAccordion>
+    );
+  },
 );
 
 export default Accordion;
--- src/components/Accordion/AccordionItem/AccordionItem.tsx
+++ src/components/Accordion/AccordionItem/AccordionItem.tsx
@@ -1,51 +1,47 @@
+import {
+  AccordionContent,
+  AccordionPanel,
+  AccordionTitle,
+} from 'flowbite-react';
 import { forwardRef } from 'react';
 
-import {
-    AccordionContent,
-    AccordionPanel,
-    AccordionTitle,
-} from 'flowbite-react';
+import { iAccordionItemProps } from '@/types/types/components/accordion';
 
-import { AccordionItemProps } from '@/types/shared/types/components/accordion';
-
-// eslint-disable-next-line react/display-name
-const AccordionItem = forwardRef<any, AccordionItemProps>(
-    (
-        {
-            id,
-            className,
-            titleClassName,
-            contentClassName,
-            title,
-            content,
-            testId,
-            onClick,
-            onMouseEnter,
-            onMouseLeave,
-            ...rest
-        }: AccordionItemProps,
-        ref: React.Ref<any> | null,
-    ) => {
-        return (
-            <AccordionPanel
-                id={id}
-                className={className}
-                data-testid={testId}
-                onClick={onClick}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                ref={ref}
-                {...rest}
-            >
-                <AccordionTitle className={titleClassName}>
-                    {title}
-                </AccordionTitle>
-                <AccordionContent className={contentClassName}>
-                    {content}
-                </AccordionContent>
-            </AccordionPanel>
-        );
-    },
+const AccordionItem = forwardRef<any, iAccordionItemProps>(
+  (
+    {
+      id,
+      className,
+      titleClassName,
+      contentClassName,
+      title,
+      content,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: iAccordionItemProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <AccordionPanel
+        id={id}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={ref}
+        {...rest}
+      >
+        <AccordionTitle className={titleClassName}>{title}</AccordionTitle>
+        <AccordionContent className={contentClassName}>
+          {content}
+        </AccordionContent>
+      </AccordionPanel>
+    );
+  },
 );
 
 export default AccordionItem;
