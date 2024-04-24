53bbaeb94809baa16d4d8eb9a8110e00bd1bc7b6
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-24 20:02:18 +0330

    feat: added rating component and sub components and their unit tests

--- src/shared/types/components/rating.ts
+++ src/shared/types/components/rating.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD:src/shared/types/components/rating.ts
 export type RatingProps = {
+=======
+export type iRatingProps = {
+>>>>>>> 62a4e96 (feat: added rating component and sub components and their unit tests):src/types/components/rating.ts
   id?: string;
   className?: string;
   children: React.ReactNode;
@@ -8,7 +12,11 @@ export type RatingProps = {
   onMouseEnter?: () => void;
   onMouseLeave?: () => void;
 };
+<<<<<<< HEAD:src/shared/types/components/rating.ts
 export type RatingStarProps = {
+=======
+export type iRatingStarProps = {
+>>>>>>> 62a4e96 (feat: added rating component and sub components and their unit tests):src/types/components/rating.ts
   id?: string;
   filled?: boolean;
   className?: string;
@@ -17,7 +25,11 @@ export type RatingStarProps = {
   onMouseEnter?: () => void;
   onMouseLeave?: () => void;
 };
+<<<<<<< HEAD:src/shared/types/components/rating.ts
 export type RatingAdvancedProps = {
+=======
+export type iRatingAdvancedProps = {
+>>>>>>> 62a4e96 (feat: added rating component and sub components and their unit tests):src/types/components/rating.ts
   id?: string;
   percentFilled: number;
   className?: string;
