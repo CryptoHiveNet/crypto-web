a0a801826f7b580d0ff2b83173b923a81477084a
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-24 19:09:49 +0330

    feat: added navbar component and sub components and their unit tests

---
+++ src/components/navbar/navbarToggle/NavbarToggle.spec.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+
+import Navbar from '../Navbar';
+import NavbarToggle from './NavbarToggle';
+
+describe('NavbarToggle component unit tests', () => {
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-navbar-toggle',
+        onClick: onClickMock,
+    };
+    const { getByTestId } = render(
+        <Navbar>
+            <NavbarToggle {...mockProps} />
+        </Navbar>,
+    );
+    it('should render NavbarToggle component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-navbar-toggle')).toBeInTheDocument();
+        });
+    });
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-navbar-toggle'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
+    });
+});
