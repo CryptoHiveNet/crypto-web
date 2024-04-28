8c6a692bb88cc63fb89dc9363996e8a67c077e9f
Amir <haghighi251@gmail.com>
2024-04-28 20:57:29 +0200

    refactor layout and add add register form

--- src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
+++ src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 const RegistrationPage = () => {
@@ -14,6 +15,23 @@ const RegistrationPage = () => {
             />
         </main>
     );
+=======
+import { Button, Checkbox, Label } from 'flowbite-react';
+import { t } from 'i18next';
+import Link from 'next/link';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+
+
+
+const RegistrationPage = () => {
+  return (
+    <main className="mx-auto w-[29.375rem] max-w-full px-4 py-12 font-base">
+            <h1 className="mb-8 font-[inherit] text-md text-grey-1">Register</h1>
+
+            <RegistrationForm submit={submit} isLoading={isLoading} errorMessages={errorMessages} />
+        </main>
+  );
+>>>>>>> 30863e1 (refactor layout and add add register form)
 };
 
 export default RegistrationPage;
