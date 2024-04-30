473d6bbec3f4a66dfaddeb1cc07b4bd345350d7b
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-30 19:11:54 +0330

    feat: dropdown modified

---
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -0,0 +1,25 @@
+import { render, waitFor } from '@testing-library/react';
+
+import Dropdown from './Dropdown';
+
+describe('Dropdown', () => {
+  const label = 'Options';
+  const size = 'large';
+  const { getByText, container } = render(
+    <Dropdown
+      label={label}
+      size={size}
+    />,
+  );
+  it('renders a dropdown with the provided label', () => {
+    waitFor(() => {
+      expect(getByText(label)).toBeInTheDocument();
+    });
+  });
+
+  it('renders a dropdown with the provided size', () => {
+    waitFor(() => {
+      expect(container.firstChild).toHaveClass(`fb-dropdown-${size}`);
+    });
+  });
+});
