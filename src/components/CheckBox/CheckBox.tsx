59e046d13957ddbbf919107e652e945aeafc7d91
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-30 19:00:58 +0330

    feat: datepicker component modified

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,40 +1,41 @@
 'use client';
-import { Checkbox as CHB } from 'flowbite-react';
 import { forwardRef } from 'react';
 
-import { iCheckBoxProps } from '@/types/shared/types/components/checkBox';
+import { Checkbox as CHB } from 'flowbite-react';
+
+import { CheckBoxProps } from '@/types/shared/types/components/checkBox';
 
 // eslint-disable-next-line react/display-name
-const CheckBox = forwardRef<any, iCheckBoxProps>(
-  (
-    {
-      id,
-      className,
-      defaultChecked,
-      disabled,
-      testId,
-      onClick,
-      onMouseEnter,
-      onMouseLeave,
-      ...rest
-    }: iCheckBoxProps,
-    ref: React.Ref<any> | null,
-  ) => {
-    return (
-      <CHB
-        id={id}
-        className={className}
-        defaultChecked={defaultChecked}
-        disabled={disabled}
-        data-testid={testId}
-        onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        ref={ref}
-        {...rest}
-      />
-    );
-  },
+const CheckBox = forwardRef<any, CheckBoxProps>(
+    (
+        {
+            id,
+            className,
+            defaultChecked,
+            disabled,
+            testId,
+            onClick,
+            onMouseEnter,
+            onMouseLeave,
+            ...rest
+        }: CheckBoxProps,
+        ref: React.Ref<any> | null,
+    ) => {
+        return (
+            <CHB
+                id={id}
+                className={className}
+                defaultChecked={defaultChecked}
+                disabled={disabled}
+                data-testid={testId}
+                onClick={onClick}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                ref={ref}
+                {...rest}
+            />
+        );
+    },
 );
 
 export default CheckBox;
