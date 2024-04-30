d0aa71847095712ff8188b23998c8cfb84645d84
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-30 15:34:43 +0330

    feat: breadcrumb and button refactored

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -5,15 +5,15 @@ import { LanguageSwitcher } from '@/types/components/i18n/LanguageSwitcher';
 import { useTranslation } from '../../utils/i18n';
 
 export default async function Page({ params: { lng } }: any) {
-  const { t } = await useTranslation();
-  return (
-    <>
-      <h1>
-        {t('name')}
-        {t('title')}
-      </h1>
-      <Link href={`/${lng}/second-page`}>{t('hello')}</Link>
-      <LanguageSwitcher />
-    </>
-  );
+    const { t } = await useTranslation();
+    return (
+        <>
+            <h1>
+                {t('name')}
+                {t('title')}
+            </h1>
+            <Link href={`/${lng}/second-page`}>{t('hello')}</Link>
+            <LanguageSwitcher />
+        </>
+    );
 }
