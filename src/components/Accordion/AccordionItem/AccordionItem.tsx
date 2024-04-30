b3587c2f49a5a7088222ca3b12d870fc3eac0662
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-30 14:00:01 +0330

    feat: accordion and avatar component refactored

---
+++ src/components/Accordion/AccordionItem/AccordionItem.tsx
@@ -0,0 +1,43 @@
+import { AccordionContent, AccordionPanel, AccordionTitle } from 'flowbite-react';
+import { forwardRef } from 'react';
+
+import { AccordionItemProps } from '@/types/types/components/accordion';
+
+const AccordionItem = forwardRef<any, AccordionItemProps>(
+  (
+    {
+      id,
+      className,
+      titleClassName,
+      contentClassName,
+      title,
+      content,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: AccordionItemProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <AccordionPanel
+        id={id}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={ref}
+        {...rest}
+      >
+        <AccordionTitle className={titleClassName}>{title}</AccordionTitle>
+        <AccordionContent className={contentClassName}>
+          {content}
+        </AccordionContent>
+      </AccordionPanel>
+    );
+  },
+);
+
+export default AccordionItem;
