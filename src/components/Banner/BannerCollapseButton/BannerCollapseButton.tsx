2ced763a3f742a6cf8227f9a58eff77725bbe239
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-30 14:38:51 +0330

    feat: banner component added

---
+++ src/components/Banner/BannerCollapseButton/BannerCollapseButton.tsx
@@ -0,0 +1,38 @@
+import { BannerCollapseButton as FlowbiteBannerCollapseButton } from 'flowbite-react';
+import { forwardRef } from 'react';
+
+import { BannerCollapseButtonProps } from '@/types/types/components/banner';
+
+const BannerCollapseButton = forwardRef<any, BannerCollapseButtonProps>(
+  (
+    {
+      id,
+      color,
+      className,
+      children,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: BannerCollapseButtonProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <FlowbiteBannerCollapseButton
+        id={id}
+        color={color}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        {...rest}
+      >
+        {children}
+      </FlowbiteBannerCollapseButton>
+    );
+  },
+);
+
+export default BannerCollapseButton;
