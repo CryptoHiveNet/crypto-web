4db6bf09f3458dd9429df2f18ecc38028a910567
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-01 16:34:25 +0330

    feat: change types folder location

--- src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
+++ src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,17 +1,16 @@
-import { Button, Checkbox, Label } from 'flowbite-react';
-import { t } from 'i18next';
-import Link from 'next/link';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
-
-
 
 const RegistrationPage = () => {
   return (
-    <main className="mx-auto w-[29.375rem] max-w-full px-4 py-12 font-base">
-            <h1 className="mb-8 font-[inherit] text-md text-grey-1">Register</h1>
+    <main className='mx-auto w-[29.375rem] max-w-full px-4 py-12 font-base'>
+      <h1 className='mb-8 font-[inherit] text-md text-grey-1'>Register</h1>
 
-            <RegistrationForm submit={submit} isLoading={isLoading} errorMessages={errorMessages} />
-        </main>
+      <RegistrationForm
+        submit={submit}
+        isLoading={isLoading}
+        errorMessages={errorMessages}
+      />
+    </main>
   );
 };
 
