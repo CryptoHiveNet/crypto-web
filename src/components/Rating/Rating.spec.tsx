d6c2dbf5e10f20539ab11423dbda6cb00c2e02f2
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-01 11:33:51 +0330

    feat: modified popover, rating, and progress components

---
+++ src/components/Rating/Rating.spec.tsx
@@ -0,0 +1,17 @@
+import { render, waitFor } from '@testing-library/react';
+
+import Rating from './Rating';
+
+describe('Rating component unit tests', () => {
+  const mockProps = {
+    id: 'test-id',
+    size: 'medium',
+    testId: 'test-rating',
+  };
+  const { getByTestId } = render(<Rating {...mockProps} />);
+  it('should render Rating component with required props', () => {
+    waitFor(() => {
+      expect(getByTestId('test-rating')).toBeInTheDocument();
+    });
+  });
+});
