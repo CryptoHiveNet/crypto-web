8b5815d35c518ffcbacce33cac2fc11a284b25a6
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-01 10:57:08 +0330

    feat: modified List and Modal components

---
+++ src/components/Modal/ModalBody/ModalBody.tsx
@@ -0,0 +1,37 @@
+import { ModalBody as FlowbiteModalBody } from 'flowbite-react';
+import { forwardRef } from 'react';
+
+import { ModalBodyProps } from '@/types/types/components/modal';
+
+const ModalBody = forwardRef<any, ModalBodyProps>(
+  (
+    {
+      id,
+      className,
+      children,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: ModalBodyProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <FlowbiteModalBody
+        id={id}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </FlowbiteModalBody>
+    );
+  },
+);
+
+export default ModalBody;
---
+++ src/components/Modal/ModalFooter/ModalFooter.tsx
@@ -0,0 +1,37 @@
+import { ModalFooter as FlowbiteModalFooter } from 'flowbite-react';
+import { forwardRef } from 'react';
+
+import { ModalFooterProps } from '@/types/types/components/modal';
+
+const ModalFooter = forwardRef<any, ModalFooterProps>(
+  (
+    {
+      id,
+      className,
+      children,
+      testId,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: ModalFooterProps,
+    ref: React.Ref<any> | null,
+  ) => {
+    return (
+      <FlowbiteModalFooter
+        id={id}
+        className={className}
+        data-testid={testId}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </FlowbiteModalFooter>
+    );
+  },
+);
+
+export default ModalFooter;
