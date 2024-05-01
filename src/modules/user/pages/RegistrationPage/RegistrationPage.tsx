5d91c9864d1809eabd8051565ef6a85b2899b1f6
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-01 16:47:56 +0330

    feat: change types folder location

--- src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
+++ src/modules/user/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,17 +1,19 @@
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 const RegistrationPage = () => {
-  return (
-    <main className='mx-auto w-[29.375rem] max-w-full px-4 py-12 font-base'>
-      <h1 className='mb-8 font-[inherit] text-md text-grey-1'>Register</h1>
+    return (
+        <main className="mx-auto w-[29.375rem] max-w-full px-4 py-12 font-base">
+            <h1 className="mb-8 font-[inherit] text-md text-grey-1">
+                Register
+            </h1>
 
-      <RegistrationForm
-        submit={submit}
-        isLoading={isLoading}
-        errorMessages={errorMessages}
-      />
-    </main>
-  );
+            <RegistrationForm
+                submit={submit}
+                isLoading={isLoading}
+                errorMessages={errorMessages}
+            />
+        </main>
+    );
 };
 
 export default RegistrationPage;
