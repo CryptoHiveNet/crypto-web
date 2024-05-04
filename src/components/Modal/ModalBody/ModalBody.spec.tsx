99b8dcf0e6af5234482428466eb61beba643fcd4
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:35:54 +0330

    fix: modified with new vscode settings

--- src/components/Modal/ModalBody/ModalBody.spec.tsx
+++ src/components/Modal/ModalBody/ModalBody.spec.tsx
@@ -4,23 +4,23 @@ import Modal from '../Modal';
 import ModalBody from './ModalBody';
 
 describe('ModalBody component unit tests', () => {
-  const mockProps = {
-    id: 'test-modal-body',
-    className: 'custom-modal-body',
-    children: 'Test Modal Body',
-    testId: 'test-modal-body',
-  };
-  const { getByTestId, getByText } = render(
-    <Modal>
-      <ModalBody {...mockProps} />
-    </Modal>,
-  );
-  it('should render ModalBody component with required props', () => {
-    waitFor(() => {
-      const modalBodyComponent = getByTestId('test-modal-body');
-      expect(modalBodyComponent).toBeInTheDocument();
-      expect(modalBodyComponent).toHaveClass('custom-modal-body');
-      expect(getByText('Test Modal Body')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-modal-body',
+        className: 'custom-modal-body',
+        children: 'Test Modal Body',
+        testId: 'test-modal-body',
+    };
+    const { getByTestId, getByText } = render(
+        <Modal>
+            <ModalBody {...mockProps} />
+        </Modal>,
+    );
+    it('should render ModalBody component with required props', () => {
+        waitFor(() => {
+            const modalBodyComponent = getByTestId('test-modal-body');
+            expect(modalBodyComponent).toBeInTheDocument();
+            expect(modalBodyComponent).toHaveClass('custom-modal-body');
+            expect(getByText('Test Modal Body')).toBeInTheDocument();
+        });
     });
-  });
 });
