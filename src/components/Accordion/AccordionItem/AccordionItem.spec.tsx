50a5ef51ec3a34bb3b5f2dca9b19037576f6ba6c
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 16:32:50 +0330

    fix: format files with new vscode configs

--- src/components/Accordion/AccordionItem/AccordionItem.spec.tsx
+++ src/components/Accordion/AccordionItem/AccordionItem.spec.tsx
@@ -4,58 +4,58 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import AccordionItem from './AccordionItem';
 
 describe('AccordionItem component unit tests', () => {
-  const props: AccordionItemProps = {
-    id: 'accordion-item-id',
-    className: 'accordion-item-class',
-    titleClassName: 'accordion-title-class',
-    contentClassName: 'accordion-content-class',
-    title: 'Accordion Title',
-    content: 'Accordion Content',
-    testId: 'accordion-item-test-id',
-    onClick: jest.fn(),
-    onMouseEnter: jest.fn(),
-    onMouseLeave: jest.fn(),
-  };
+    const props: AccordionItemProps = {
+        id: 'accordion-item-id',
+        className: 'accordion-item-class',
+        titleClassName: 'accordion-title-class',
+        contentClassName: 'accordion-content-class',
+        title: 'Accordion Title',
+        content: 'Accordion Content',
+        testId: 'accordion-item-test-id',
+        onClick: jest.fn(),
+        onMouseEnter: jest.fn(),
+        onMouseLeave: jest.fn(),
+    };
 
-  it('renders correctly', () => {
-    const { getByText } = render(<AccordionItem {...props} />);
-    waitFor(() => {
-      expect(getByText('Accordion Title')).toBeInTheDocument();
-      expect(getByText('Accordion Content')).toBeInTheDocument();
+    it('renders correctly', () => {
+        const { getByText } = render(<AccordionItem {...props} />);
+        waitFor(() => {
+            expect(getByText('Accordion Title')).toBeInTheDocument();
+            expect(getByText('Accordion Content')).toBeInTheDocument();
+        });
     });
-  });
 
-  it('receives props correctly', () => {
-    const { getByTestId } = render(<AccordionItem {...props} />);
-    waitFor(() => {
-      expect(getByTestId('accordion-item-test-id')).toBeInTheDocument();
+    it('receives props correctly', () => {
+        const { getByTestId } = render(<AccordionItem {...props} />);
+        waitFor(() => {
+            expect(getByTestId('accordion-item-test-id')).toBeInTheDocument();
+        });
     });
-  });
 
-  it('calls onClick handler', () => {
-    const { getByTestId } = render(<AccordionItem {...props} />);
-    waitFor(() => {
-      const accordionItem = getByTestId('accordion-item-test-id');
-      fireEvent.click(accordionItem);
-      expect(props.onClick).toHaveBeenCalledTimes(1);
+    it('calls onClick handler', () => {
+        const { getByTestId } = render(<AccordionItem {...props} />);
+        waitFor(() => {
+            const accordionItem = getByTestId('accordion-item-test-id');
+            fireEvent.click(accordionItem);
+            expect(props.onClick).toHaveBeenCalledTimes(1);
+        });
     });
-  });
 
-  it('calls onMouseEnter handler', () => {
-    const { getByTestId } = render(<AccordionItem {...props} />);
-    waitFor(() => {
-      const accordionItem = getByTestId('accordion-item-test-id');
-      fireEvent.mouseEnter(accordionItem);
-      expect(props.onMouseEnter).toHaveBeenCalledTimes(1);
+    it('calls onMouseEnter handler', () => {
+        const { getByTestId } = render(<AccordionItem {...props} />);
+        waitFor(() => {
+            const accordionItem = getByTestId('accordion-item-test-id');
+            fireEvent.mouseEnter(accordionItem);
+            expect(props.onMouseEnter).toHaveBeenCalledTimes(1);
+        });
     });
-  });
 
-  it('calls onMouseLeave handler', () => {
-    const { getByTestId } = render(<AccordionItem {...props} />);
-    waitFor(() => {
-      const accordionItem = getByTestId('accordion-item-test-id');
-      fireEvent.mouseLeave(accordionItem);
-      expect(props.onMouseLeave).toHaveBeenCalledTimes(1);
+    it('calls onMouseLeave handler', () => {
+        const { getByTestId } = render(<AccordionItem {...props} />);
+        waitFor(() => {
+            const accordionItem = getByTestId('accordion-item-test-id');
+            fireEvent.mouseLeave(accordionItem);
+            expect(props.onMouseLeave).toHaveBeenCalledTimes(1);
+        });
     });
-  });
 });
