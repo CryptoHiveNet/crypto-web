7ad4edc0f19ea3a52781cd76b37d3075f03762ea
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 16:32:50 +0330

    fix: format files with new vscode configs

--- src/components/Avatar/AvatarCounter/AvatarCounter.spec.tsx
+++ src/components/Avatar/AvatarCounter/AvatarCounter.spec.tsx
@@ -3,24 +3,24 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import AvatarCounter from './AvatarCounter';
 
 describe('AvatarCounter component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    total: 5,
-    testId: 'test-avatar-counter',
-    onClick: onClickMock,
-  };
-  it('should render AvatarCounter component with required props', () => {
-    const { getByTestId } = render(<AvatarCounter {...mockProps} />);
-    waitFor(() => {
-      expect(getByTestId('test-avatar-counter')).toBeInTheDocument();
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        total: 5,
+        testId: 'test-avatar-counter',
+        onClick: onClickMock,
+    };
+    it('should render AvatarCounter component with required props', () => {
+        const { getByTestId } = render(<AvatarCounter {...mockProps} />);
+        waitFor(() => {
+            expect(getByTestId('test-avatar-counter')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    const { getByTestId } = render(<AvatarCounter {...mockProps} />);
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-avatar-counter'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        const { getByTestId } = render(<AvatarCounter {...mockProps} />);
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-avatar-counter'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
 });
--- src/components/Avatar/AvatarGroup/AvatarGroup.spec.tsx
+++ src/components/Avatar/AvatarGroup/AvatarGroup.spec.tsx
@@ -4,37 +4,37 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import AvatarGroup from './AvatarGroup';
 
 describe('AvatarGroup component unit tests', () => {
-  const mockChildren = (
-    <div>
-      <span>{test_children_text} 1</span>
-      <span>{test_children_text} 2</span>
-    </div>
-  );
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    testId: 'test-avatar-group',
-    onClick: onClickMock,
-  };
-  it('should render AvatarGroup component with required props', () => {
-    const { getByTestId } = render(<AvatarGroup {...mockProps} />);
+    const mockChildren = (
+        <div>
+            <span>{test_children_text} 1</span>
+            <span>{test_children_text} 2</span>
+        </div>
+    );
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-avatar-group',
+        onClick: onClickMock,
+    };
+    it('should render AvatarGroup component with required props', () => {
+        const { getByTestId } = render(<AvatarGroup {...mockProps} />);
 
-    waitFor(() => {
-      expect(getByTestId('test-avatar-group')).toBeInTheDocument();
+        waitFor(() => {
+            expect(getByTestId('test-avatar-group')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    const { getByTestId } = render(<AvatarGroup {...mockProps} />);
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-avatar-group'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        const { getByTestId } = render(<AvatarGroup {...mockProps} />);
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-avatar-group'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
-  it('should render children', () => {
-    const { getByText } = render(<AvatarGroup>{mockChildren}</AvatarGroup>);
-    waitFor(() => {
-      expect(getByText(`${test_children_text} 1`)).toBeInTheDocument();
-      expect(getByText(`${test_children_text} 2`)).toBeInTheDocument();
+    it('should render children', () => {
+        const { getByText } = render(<AvatarGroup>{mockChildren}</AvatarGroup>);
+        waitFor(() => {
+            expect(getByText(`${test_children_text} 1`)).toBeInTheDocument();
+            expect(getByText(`${test_children_text} 2`)).toBeInTheDocument();
+        });
     });
-  });
 });
