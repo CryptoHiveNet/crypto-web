2a6c479fe07b827c968ceaa5460fbeb6a9b3fd24
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:16:55 +0330

    fix: modified with new vscode settings

--- src/components/Datepicker/Datepicker.spec.tsx
+++ src/components/Datepicker/Datepicker.spec.tsx
@@ -3,24 +3,24 @@ import { render, waitFor } from '@testing-library/react';
 import Datepicker from './Datepicker';
 
 describe('Datepicker component unit tests', () => {
-  const labelTodayButton = 'Today';
-  const labelClearButton = 'Clear';
-  const { getByText } = render(
-    <Datepicker
-      labelTodayButton={labelTodayButton}
-      labelClearButton={labelClearButton}
-    />,
-  );
+    const labelTodayButton = 'Today';
+    const labelClearButton = 'Clear';
+    const { getByText } = render(
+        <Datepicker
+            labelTodayButton={labelTodayButton}
+            labelClearButton={labelClearButton}
+        />,
+    );
 
-  it('renders a datepicker with the provided label for today button', () => {
-    waitFor(() => {
-      expect(getByText(labelTodayButton)).toBeInTheDocument();
+    it('renders a datepicker with the provided label for today button', () => {
+        waitFor(() => {
+            expect(getByText(labelTodayButton)).toBeInTheDocument();
+        });
     });
-  });
 
-  it('renders a datepicker with the provided label for clear button', () => {
-    waitFor(() => {
-      expect(getByText(labelClearButton)).toBeInTheDocument();
+    it('renders a datepicker with the provided label for clear button', () => {
+        waitFor(() => {
+            expect(getByText(labelClearButton)).toBeInTheDocument();
+        });
     });
-  });
 });
