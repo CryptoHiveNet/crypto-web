e6759b8f590422ff03f71f728c01a742b3780ba4
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 16:58:29 +0330

    fix: modified with new vscode settings

--- src/components/Breadcrumb/BreadcrumbItem/BreadcrumbItem.spec.tsx
+++ src/components/Breadcrumb/BreadcrumbItem/BreadcrumbItem.spec.tsx
@@ -4,30 +4,30 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import BreadcrumbItem from './BreadcrumbItem';
 
 describe('BreadcrumbItem component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    href: '#home',
-    testId: 'test-breadcrumb-item',
-    onClick: onClickMock,
-  };
-  const { getByTestId, getByText } = render(
-    <BreadcrumbItem {...mockProps}>{test_children_text}</BreadcrumbItem>,
-  );
-  it('should render BreadcrumbItem component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-breadcrumb-item')).toBeInTheDocument();
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        href: '#home',
+        testId: 'test-breadcrumb-item',
+        onClick: onClickMock,
+    };
+    const { getByTestId, getByText } = render(
+        <BreadcrumbItem {...mockProps}>{test_children_text}</BreadcrumbItem>,
+    );
+    it('should render BreadcrumbItem component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-breadcrumb-item')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-breadcrumb-item'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-breadcrumb-item'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
-  it('should render children', () => {
-    waitFor(() => {
-      expect(getByText(test_children_text)).toBeInTheDocument();
+    it('should render children', () => {
+        waitFor(() => {
+            expect(getByText(test_children_text)).toBeInTheDocument();
+        });
     });
-  });
 });
--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -4,61 +4,61 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button component tests', () => {
-  const handleClick = jest.fn();
-  const onMouseLeaveMock = jest.fn();
-  const onMouseEnterMock = jest.fn();
-  const props = {
-    testId: 'my-button',
-    onClick: handleClick,
-    onMouseEnter: onMouseEnterMock,
-    onMouseLeave: onMouseLeaveMock,
-  };
+    const handleClick = jest.fn();
+    const onMouseLeaveMock = jest.fn();
+    const onMouseEnterMock = jest.fn();
+    const props = {
+        testId: 'my-button',
+        onClick: handleClick,
+        onMouseEnter: onMouseEnterMock,
+        onMouseLeave: onMouseLeaveMock,
+    };
 
-  const { getByTestId, getByText } = render(
-    <Button {...props}>{test_children_text}</Button>,
-  );
-  it('renders children correctly', () => {
-    expect(getByText(test_children_text)).toBeInTheDocument();
-  });
-
-  it('executes onClick handler when clicked', () => {
-    waitFor(() => {
-      fireEvent.click(getByText(test_children_text));
-      fireEvent.click(getByText(test_children_text));
-      expect(handleClick).toHaveBeenCalledTimes(2);
-    });
-  });
-
-  it('renders as disabled when disabled prop is true', () => {
-    const { container } = render(<Button disabled>Disabled Button</Button>);
-    expect(container.firstChild).toBeDisabled();
-  });
-
-  it('renders a processing spinner when isProcessing prop is true', () => {
-    const { getByTestId } = render(
-      <Button
-        isProcessing={true}
-        processingSpinner={<div data-testid='spinner'>Spinner</div>}
-      >
-        Submit
-      </Button>,
+    const { getByTestId, getByText } = render(
+        <Button {...props}>{test_children_text}</Button>,
     );
-    expect(getByTestId('spinner')).toBeInTheDocument();
-  });
-
-  it('should call onMouseEnter when mouse enters', () => {
-    waitFor(() => {
-      const buttonComponent = getByTestId('my-button');
-      fireEvent.mouseEnter(buttonComponent);
-      expect(onMouseEnterMock).toHaveBeenCalled();
+    it('renders children correctly', () => {
+        expect(getByText(test_children_text)).toBeInTheDocument();
     });
-  });
 
-  it('should call onMouseLeave when mouse leaves', () => {
-    waitFor(() => {
-      const buttonComponent = getByTestId('my-button');
-      fireEvent.mouseLeave(buttonComponent);
-      expect(onMouseLeaveMock).toHaveBeenCalled();
+    it('executes onClick handler when clicked', () => {
+        waitFor(() => {
+            fireEvent.click(getByText(test_children_text));
+            fireEvent.click(getByText(test_children_text));
+            expect(handleClick).toHaveBeenCalledTimes(2);
+        });
     });
-  });
+
+    it('renders as disabled when disabled prop is true', () => {
+        const { container } = render(<Button disabled>Disabled Button</Button>);
+        expect(container.firstChild).toBeDisabled();
+    });
+
+    it('renders a processing spinner when isProcessing prop is true', () => {
+        const { getByTestId } = render(
+            <Button
+                isProcessing={true}
+                processingSpinner={<div data-testid="spinner">Spinner</div>}
+            >
+                Submit
+            </Button>,
+        );
+        expect(getByTestId('spinner')).toBeInTheDocument();
+    });
+
+    it('should call onMouseEnter when mouse enters', () => {
+        waitFor(() => {
+            const buttonComponent = getByTestId('my-button');
+            fireEvent.mouseEnter(buttonComponent);
+            expect(onMouseEnterMock).toHaveBeenCalled();
+        });
+    });
+
+    it('should call onMouseLeave when mouse leaves', () => {
+        waitFor(() => {
+            const buttonComponent = getByTestId('my-button');
+            fireEvent.mouseLeave(buttonComponent);
+            expect(onMouseLeaveMock).toHaveBeenCalled();
+        });
+    });
 });
