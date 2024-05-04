07ad55523364301c0a4510d763fa5c0c0a1c7f59
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:54:42 +0330

    fix: modified with new vscode settings

--- src/components/Spinner/Spinner.spec.tsx
+++ src/components/Spinner/Spinner.spec.tsx
@@ -3,21 +3,24 @@ import { render, waitFor } from '@testing-library/react';
 import Spinner from './Spinner';
 
 describe('Spinner component unit tests', () => {
-  const mockProps = {
-    id: 'test-spinner',
-    ariaLabel: 'Loading spinner',
-    color: 'blue',
-    size: 'medium',
-    className: 'custom-spinner',
-    testId: 'test-spinner',
-  };
-  const { getByTestId } = render(<Spinner {...mockProps} />);
-  it('should render Spinner component with required props', () => {
-    waitFor(() => {
-      const spinnerComponent = getByTestId('test-spinner');
-      expect(spinnerComponent).toBeInTheDocument();
-      expect(spinnerComponent).toHaveAttribute('aria-label', 'Loading spinner');
-      expect(spinnerComponent).toHaveClass('custom-spinner');
+    const mockProps = {
+        id: 'test-spinner',
+        ariaLabel: 'Loading spinner',
+        color: 'blue',
+        size: 'medium',
+        className: 'custom-spinner',
+        testId: 'test-spinner',
+    };
+    const { getByTestId } = render(<Spinner {...mockProps} />);
+    it('should render Spinner component with required props', () => {
+        waitFor(() => {
+            const spinnerComponent = getByTestId('test-spinner');
+            expect(spinnerComponent).toBeInTheDocument();
+            expect(spinnerComponent).toHaveAttribute(
+                'aria-label',
+                'Loading spinner',
+            );
+            expect(spinnerComponent).toHaveClass('custom-spinner');
+        });
     });
-  });
 });
