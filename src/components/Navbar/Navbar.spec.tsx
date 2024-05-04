b5b7c3ac8c6d8e322e2788dd7c756ba96e4b4aed
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:40:57 +0330

    fix: modified with new vscode settings

--- src/components/Navbar/Navbar.spec.tsx
+++ src/components/Navbar/Navbar.spec.tsx
@@ -3,30 +3,30 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import Navbar from './Navbar';
 
 describe('Navbar component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    testId: 'test-navbar',
-    fluid: true,
-    rounded: false,
-    onClick: onClickMock,
-  };
-  const { getByTestId } = render(<Navbar {...mockProps} />);
-  it('should render Navbar component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-navbar')).toBeInTheDocument();
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-navbar',
+        fluid: true,
+        rounded: false,
+        onClick: onClickMock,
+    };
+    const { getByTestId } = render(<Navbar {...mockProps} />);
+    it('should render Navbar component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-navbar')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-navbar'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-navbar'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
-  it('should render fluid and rounded props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-navbar')).toHaveClass('fluid');
-      expect(getByTestId('test-navbar')).not.toHaveClass('rounded');
+    it('should render fluid and rounded props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-navbar')).toHaveClass('fluid');
+            expect(getByTestId('test-navbar')).not.toHaveClass('rounded');
+        });
     });
-  });
 });
