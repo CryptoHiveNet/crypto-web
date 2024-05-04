92648bd62bf2ec47eb076913e9e3e8550d4f294f
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:40:57 +0330

    fix: modified with new vscode settings

--- src/components/Navbar/NavbarLink/NavbarLink.spec.tsx
+++ src/components/Navbar/NavbarLink/NavbarLink.spec.tsx
@@ -4,29 +4,32 @@ import Navbar from '../Navbar';
 import NavbarLink from './NavbarLink';
 
 describe('NavbarLink component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    href: '#',
-    testId: 'test-navbar-link',
-    onClick: onClickMock,
-  };
-  const { getByTestId } = render(
-    <Navbar>
-      <NavbarLink {...mockProps}>Link</NavbarLink>
-    </Navbar>,
-  );
-  it('should render NavbarLink component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-navbar-link')).toBeInTheDocument();
-      expect(getByTestId('test-navbar-link')).toHaveAttribute('href', '#');
-      expect(getByTestId('test-navbar-link')).toHaveTextContent('Link');
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        href: '#',
+        testId: 'test-navbar-link',
+        onClick: onClickMock,
+    };
+    const { getByTestId } = render(
+        <Navbar>
+            <NavbarLink {...mockProps}>Link</NavbarLink>
+        </Navbar>,
+    );
+    it('should render NavbarLink component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-navbar-link')).toBeInTheDocument();
+            expect(getByTestId('test-navbar-link')).toHaveAttribute(
+                'href',
+                '#',
+            );
+            expect(getByTestId('test-navbar-link')).toHaveTextContent('Link');
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-navbar-link'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-navbar-link'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
 });
