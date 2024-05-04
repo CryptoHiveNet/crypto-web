14745dfa54877b91c17bf5cc16f426358992ecd3
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:16:55 +0330

    fix: modified with new vscode settings

--- src/components/Datepicker/Datepicker.tsx
+++ src/components/Datepicker/Datepicker.tsx
@@ -1,53 +1,54 @@
-import { Datepicker as FlowbiteDatepicker } from 'flowbite-react';
 import { forwardRef } from 'react';
+
+import { Datepicker as FlowbiteDatepicker } from 'flowbite-react';
 
 import { DatepickerProps } from '@/types/shared/types/components/datepicker';
 
 // eslint-disable-next-line react/display-name
 const Datepicker = forwardRef<any, DatepickerProps>(
-  (
-    {
-      id,
-      language,
-      labelTodayButton,
-      labelClearButton,
-      minDate,
-      maxDate,
-      weekStart,
-      autoHide,
-      title,
-      inline,
-      className,
-      testId,
-      onClick,
-      onMouseEnter,
-      onMouseLeave,
-      ...rest
-    }: DatepickerProps,
-    ref: React.Ref<any> | null,
-  ) => {
-    return (
-      <FlowbiteDatepicker
-        id={id}
-        language={language}
-        labelTodayButton={labelTodayButton}
-        labelClearButton={labelClearButton}
-        minDate={minDate}
-        maxDate={maxDate}
-        weekStart={weekStart}
-        autoHide={autoHide}
-        title={title}
-        inline={inline}
-        className={className}
-        data-testid={testId}
-        onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        // ref={ref}
-        {...rest}
-      />
-    );
-  },
+    (
+        {
+            id,
+            language,
+            labelTodayButton,
+            labelClearButton,
+            minDate,
+            maxDate,
+            weekStart,
+            autoHide,
+            title,
+            inline,
+            className,
+            testId,
+            onClick,
+            onMouseEnter,
+            onMouseLeave,
+            ...rest
+        }: DatepickerProps,
+        ref: React.Ref<any> | null,
+    ) => {
+        return (
+            <FlowbiteDatepicker
+                id={id}
+                language={language}
+                labelTodayButton={labelTodayButton}
+                labelClearButton={labelClearButton}
+                minDate={minDate}
+                maxDate={maxDate}
+                weekStart={weekStart}
+                autoHide={autoHide}
+                title={title}
+                inline={inline}
+                className={className}
+                data-testid={testId}
+                onClick={onClick}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                // ref={ref}
+                {...rest}
+            />
+        );
+    },
 );
 
 export default Datepicker;
