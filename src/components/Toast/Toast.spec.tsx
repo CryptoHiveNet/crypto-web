62f2f3ba4c37fc0ba20ccc053f7b6178651e3d57
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:57:59 +0330

    fix: modified with new vscode settings

--- src/components/Toast/Toast.spec.tsx
+++ src/components/Toast/Toast.spec.tsx
@@ -3,18 +3,18 @@ import { render, waitFor } from '@testing-library/react';
 import Toast from './Toast';
 
 describe('Toast component unit tests', () => {
-  const mockProps = {
-    id: 'test-toast',
-    className: 'custom-toast',
-    testId: 'test-toast',
-  };
-  const { getByTestId } = render(<Toast {...mockProps}>Test message</Toast>);
-  it('should render Toast component with required props', () => {
-    waitFor(() => {
-      const toastComponent = getByTestId('test-toast');
-      expect(toastComponent).toBeInTheDocument();
-      expect(toastComponent).toHaveTextContent('Test message');
-      expect(toastComponent).toHaveClass('custom-toast');
+    const mockProps = {
+        id: 'test-toast',
+        className: 'custom-toast',
+        testId: 'test-toast',
+    };
+    const { getByTestId } = render(<Toast {...mockProps}>Test message</Toast>);
+    it('should render Toast component with required props', () => {
+        waitFor(() => {
+            const toastComponent = getByTestId('test-toast');
+            expect(toastComponent).toBeInTheDocument();
+            expect(toastComponent).toHaveTextContent('Test message');
+            expect(toastComponent).toHaveClass('custom-toast');
+        });
     });
-  });
 });
