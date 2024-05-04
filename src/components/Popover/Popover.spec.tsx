bbdd0599af2ab6ad9cd06fd65d18b86179183b83
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:46:42 +0330

    fix: modified with new vscode settings

--- src/components/Popover/Popover.spec.tsx
+++ src/components/Popover/Popover.spec.tsx
@@ -3,39 +3,39 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import Popover from './Popover';
 
 describe('Popover component unit tests', () => {
-  const mockProps = {
-    id: 'test-popover',
-    ariaLabelledby: 'popover-title',
-    content: 'Popover content',
-    trigger: 'click',
-    open: false,
-    onOpenChange: jest.fn(),
-    placement: 'bottom',
-    arrow: true,
-    className: 'custom-popover',
-    testId: 'test-popover',
-  };
-  const { getByTestId, getByText } = render(
-    <Popover {...mockProps}>
-      <button
-        id='popover-trigger'
-        aria-labelledby='popover-title'
-      >
-        Trigger
-      </button>
-    </Popover>,
-  );
-  it('should render Popover component with required props', () => {
-    waitFor(() => {
-      const popoverComponent = getByTestId('test-popover');
-      expect(popoverComponent).toBeInTheDocument();
-      expect(popoverComponent).toHaveClass('custom-popover');
+    const mockProps = {
+        id: 'test-popover',
+        ariaLabelledby: 'popover-title',
+        content: 'Popover content',
+        trigger: 'click',
+        open: false,
+        onOpenChange: jest.fn(),
+        placement: 'bottom',
+        arrow: true,
+        className: 'custom-popover',
+        testId: 'test-popover',
+    };
+    const { getByTestId, getByText } = render(
+        <Popover {...mockProps}>
+            <button
+                id="popover-trigger"
+                aria-labelledby="popover-title"
+            >
+                Trigger
+            </button>
+        </Popover>,
+    );
+    it('should render Popover component with required props', () => {
+        waitFor(() => {
+            const popoverComponent = getByTestId('test-popover');
+            expect(popoverComponent).toBeInTheDocument();
+            expect(popoverComponent).toHaveClass('custom-popover');
+        });
     });
-  });
-  it('should toggle open state when trigger element is clicked', () => {
-    waitFor(() => {
-      fireEvent.click(getByText('Trigger'));
-      expect(mockProps.onOpenChange).toHaveBeenCalledTimes(1);
+    it('should toggle open state when trigger element is clicked', () => {
+        waitFor(() => {
+            fireEvent.click(getByText('Trigger'));
+            expect(mockProps.onOpenChange).toHaveBeenCalledTimes(1);
+        });
     });
-  });
 });
--- src/components/Progress/Progress.spec.tsx
+++ src/components/Progress/Progress.spec.tsx
@@ -3,15 +3,15 @@ import { render, waitFor } from '@testing-library/react';
 import Progress from './Progress';
 
 describe('Progress component unit tests', () => {
-  const mockProps = {
-    id: 'test-id',
-    progress: 50,
-    testId: 'test-progress',
-  };
-  const { getByTestId } = render(<Progress {...mockProps} />);
-  it('should render Progress component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-progress')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-id',
+        progress: 50,
+        testId: 'test-progress',
+    };
+    const { getByTestId } = render(<Progress {...mockProps} />);
+    it('should render Progress component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-progress')).toBeInTheDocument();
+        });
     });
-  });
 });
