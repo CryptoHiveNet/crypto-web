40a0545341a4f5a2e83be42a131e2e342ff7b83c
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:35:54 +0330

    fix: modified with new vscode settings

--- src/components/ListGroup/ListGroupItem/ListGroupItem.spec.tsx
+++ src/components/ListGroup/ListGroupItem/ListGroupItem.spec.tsx
@@ -3,30 +3,30 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import ListGroupItem from './ListGroupItem';
 
 describe('ListGroupItem component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    testId: 'test-list-group-item',
-    onClick: onClickMock,
-  };
-  const mockChildren = <div>Test Children</div>;
-  const { getByTestId, getByText } = render(
-    <ListGroupItem {...mockProps}>{mockChildren}</ListGroupItem>,
-  );
-  it('should render ListGroupItem component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-list-group-item')).toBeInTheDocument();
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-list-group-item',
+        onClick: onClickMock,
+    };
+    const mockChildren = <div>Test Children</div>;
+    const { getByTestId, getByText } = render(
+        <ListGroupItem {...mockProps}>{mockChildren}</ListGroupItem>,
+    );
+    it('should render ListGroupItem component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-list-group-item')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-list-group-item'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-list-group-item'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
-  it('should render children', () => {
-    waitFor(() => {
-      expect(getByText('Test Children')).toBeInTheDocument();
+    it('should render children', () => {
+        waitFor(() => {
+            expect(getByText('Test Children')).toBeInTheDocument();
+        });
     });
-  });
 });
--- src/components/Modal/Modal.spec.tsx
+++ src/components/Modal/Modal.spec.tsx
@@ -3,32 +3,32 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import Modal from './Modal';
 
 describe('Modal component unit tests', () => {
-  const onCloseMock = jest.fn();
-  const mockProps = {
-    id: 'test-modal',
-    show: true,
-    onClose: onCloseMock,
-    dismissible: true,
-    size: 'large',
-    popup: true,
-    initialFocus: 'button',
-    position: 'center',
-    className: 'custom-modal',
-    testId: 'test-modal',
-  };
-  const { getByTestId } = render(<Modal {...mockProps} />);
-  it('should render Modal component with required props', () => {
-    waitFor(() => {
-      const modalComponent = getByTestId('test-modal');
-      expect(modalComponent).toBeInTheDocument();
-      expect(modalComponent).toHaveClass('custom-modal');
+    const onCloseMock = jest.fn();
+    const mockProps = {
+        id: 'test-modal',
+        show: true,
+        onClose: onCloseMock,
+        dismissible: true,
+        size: 'large',
+        popup: true,
+        initialFocus: 'button',
+        position: 'center',
+        className: 'custom-modal',
+        testId: 'test-modal',
+    };
+    const { getByTestId } = render(<Modal {...mockProps} />);
+    it('should render Modal component with required props', () => {
+        waitFor(() => {
+            const modalComponent = getByTestId('test-modal');
+            expect(modalComponent).toBeInTheDocument();
+            expect(modalComponent).toHaveClass('custom-modal');
+        });
     });
-  });
-  it('should execute onClose function when the modal is closed', () => {
-    waitFor(() => {
-      const modalComponent = getByTestId('test-modal');
-      fireEvent.click(modalComponent);
-      expect(onCloseMock).toHaveBeenCalledTimes(1);
+    it('should execute onClose function when the modal is closed', () => {
+        waitFor(() => {
+            const modalComponent = getByTestId('test-modal');
+            fireEvent.click(modalComponent);
+            expect(onCloseMock).toHaveBeenCalledTimes(1);
+        });
     });
-  });
 });
