8a6d988684be0c937c1e57f4cc1d0a2259793487
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:51:45 +0330

    fix: modified with new vscode settings

--- src/components/Sidebar/SidebarItemGroup/SidebarItemGroup.tsx
+++ src/components/Sidebar/SidebarItemGroup/SidebarItemGroup.tsx
@@ -1,38 +1,39 @@
-import { SidebarItemGroup as FlowbiteSidebarItemGroup } from 'flowbite-react';
 import { forwardRef } from 'react';
+
+import { SidebarItemGroup as FlowbiteSidebarItemGroup } from 'flowbite-react';
 
 import { SidebarItemGroupProps } from '@/types/shared/types/components/sidebar';
 
 // eslint-disable-next-line react/display-name
 const SidebarItemGroup = forwardRef<any, SidebarItemGroupProps>(
-  (
-    {
-      id,
-      className,
-      children,
-      testId,
-      onClick,
-      onMouseEnter,
-      onMouseLeave,
-      ...rest
-    }: SidebarItemGroupProps,
-    ref: React.Ref<any> | null,
-  ) => {
-    return (
-      <FlowbiteSidebarItemGroup
-        id={id}
-        className={className}
-        data-testid={testId}
-        onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        ref={ref}
-        {...rest}
-      >
-        {children}
-      </FlowbiteSidebarItemGroup>
-    );
-  },
+    (
+        {
+            id,
+            className,
+            children,
+            testId,
+            onClick,
+            onMouseEnter,
+            onMouseLeave,
+            ...rest
+        }: SidebarItemGroupProps,
+        ref: React.Ref<any> | null,
+    ) => {
+        return (
+            <FlowbiteSidebarItemGroup
+                id={id}
+                className={className}
+                data-testid={testId}
+                onClick={onClick}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                ref={ref}
+                {...rest}
+            >
+                {children}
+            </FlowbiteSidebarItemGroup>
+        );
+    },
 );
 
 export default SidebarItemGroup;
