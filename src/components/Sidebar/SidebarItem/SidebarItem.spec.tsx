938987c55640735f5de93db360a6e3588c1f813f
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:51:45 +0330

    fix: modified with new vscode settings

--- src/components/Sidebar/SidebarItem/SidebarItem.spec.tsx
+++ src/components/Sidebar/SidebarItem/SidebarItem.spec.tsx
@@ -6,27 +6,27 @@ import SidebarItems from '../SidebarItems/SidebarItems';
 import SidebarItem from './SidebarItem';
 
 describe('SidebarItem component unit tests', () => {
-  const mockProps = {
-    id: 'test-sidebar-item',
-    href: '#',
-    icon: 'icon-name',
-    label: 'Test Label',
-    labelColor: 'blue',
-    className: 'sidebar-item',
-    testId: 'test-sidebar-item',
-  };
-  const { getByTestId } = render(
-    <Sidebar>
-      <SidebarItemGroup>
-        <SidebarItems>
-          <SidebarItem {...mockProps} />
-        </SidebarItems>
-      </SidebarItemGroup>
-    </Sidebar>,
-  );
-  it('should render SidebarItem component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-sidebar-item')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-sidebar-item',
+        href: '#',
+        icon: 'icon-name',
+        label: 'Test Label',
+        labelColor: 'blue',
+        className: 'sidebar-item',
+        testId: 'test-sidebar-item',
+    };
+    const { getByTestId } = render(
+        <Sidebar>
+            <SidebarItemGroup>
+                <SidebarItems>
+                    <SidebarItem {...mockProps} />
+                </SidebarItems>
+            </SidebarItemGroup>
+        </Sidebar>,
+    );
+    it('should render SidebarItem component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-sidebar-item')).toBeInTheDocument();
+        });
     });
-  });
 });
