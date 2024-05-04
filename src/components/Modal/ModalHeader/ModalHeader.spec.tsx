b17901285f9855b3699493dcf957f7b2ebf1097f
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:35:54 +0330

    fix: modified with new vscode settings

--- src/components/Modal/ModalHeader/ModalHeader.spec.tsx
+++ src/components/Modal/ModalHeader/ModalHeader.spec.tsx
@@ -4,23 +4,23 @@ import Modal from '../Modal';
 import ModalHeader from './ModalHeader';
 
 describe('ModalHeader component unit tests', () => {
-  const mockProps = {
-    id: 'test-modal-header',
-    className: 'custom-modal-header',
-    children: 'Test Modal Header',
-    testId: 'test-modal-header',
-  };
-  const { getByTestId, getByText } = render(
-    <Modal>
-      <ModalHeader {...mockProps} />
-    </Modal>,
-  );
-  it('should render ModalHeader component with required props', () => {
-    waitFor(() => {
-      const modalHeaderComponent = getByTestId('test-modal-header');
-      expect(modalHeaderComponent).toBeInTheDocument();
-      expect(modalHeaderComponent).toHaveClass('custom-modal-header');
-      expect(getByText('Test Modal Header')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-modal-header',
+        className: 'custom-modal-header',
+        children: 'Test Modal Header',
+        testId: 'test-modal-header',
+    };
+    const { getByTestId, getByText } = render(
+        <Modal>
+            <ModalHeader {...mockProps} />
+        </Modal>,
+    );
+    it('should render ModalHeader component with required props', () => {
+        waitFor(() => {
+            const modalHeaderComponent = getByTestId('test-modal-header');
+            expect(modalHeaderComponent).toBeInTheDocument();
+            expect(modalHeaderComponent).toHaveClass('custom-modal-header');
+            expect(getByText('Test Modal Header')).toBeInTheDocument();
+        });
     });
-  });
 });
