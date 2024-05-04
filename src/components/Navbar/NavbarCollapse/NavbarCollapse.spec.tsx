29419d391565349c349c5b14fb695bea9ffcb922
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:40:57 +0330

    fix: modified with new vscode settings

--- src/components/Navbar/NavbarCollapse/NavbarCollapse.spec.tsx
+++ src/components/Navbar/NavbarCollapse/NavbarCollapse.spec.tsx
@@ -4,26 +4,26 @@ import Navbar from '../Navbar';
 import NavbarCollapse from './NavbarCollapse';
 
 describe('NavbarCollapse component unit tests', () => {
-  const onClickMock = jest.fn();
-  const mockProps = {
-    id: 'test-id',
-    testId: 'test-navbar-collapse',
-    onClick: onClickMock,
-  };
-  const { getByTestId } = render(
-    <Navbar>
-      <NavbarCollapse {...mockProps} />
-    </Navbar>,
-  );
-  it('should render NavbarCollapse component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-navbar-collapse')).toBeInTheDocument();
+    const onClickMock = jest.fn();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-navbar-collapse',
+        onClick: onClickMock,
+    };
+    const { getByTestId } = render(
+        <Navbar>
+            <NavbarCollapse {...mockProps} />
+        </Navbar>,
+    );
+    it('should render NavbarCollapse component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-navbar-collapse')).toBeInTheDocument();
+        });
     });
-  });
-  it('should handle onClick event', () => {
-    waitFor(() => {
-      fireEvent.click(getByTestId('test-navbar-collapse'));
-      expect(onClickMock).toHaveBeenCalled();
+    it('should handle onClick event', () => {
+        waitFor(() => {
+            fireEvent.click(getByTestId('test-navbar-collapse'));
+            expect(onClickMock).toHaveBeenCalled();
+        });
     });
-  });
 });
