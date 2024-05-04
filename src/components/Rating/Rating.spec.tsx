044eed67b1aff8583462ee3a1b3d80eee51ecd8c
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:46:42 +0330

    fix: modified with new vscode settings

--- src/components/Rating/Rating.spec.tsx
+++ src/components/Rating/Rating.spec.tsx
@@ -3,15 +3,15 @@ import { render, waitFor } from '@testing-library/react';
 import Rating from './Rating';
 
 describe('Rating component unit tests', () => {
-  const mockProps = {
-    id: 'test-id',
-    size: 'medium',
-    testId: 'test-rating',
-  };
-  const { getByTestId } = render(<Rating {...mockProps} />);
-  it('should render Rating component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-rating')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-id',
+        size: 'medium',
+        testId: 'test-rating',
+    };
+    const { getByTestId } = render(<Rating {...mockProps} />);
+    it('should render Rating component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-rating')).toBeInTheDocument();
+        });
     });
-  });
 });
--- src/components/Rating/RatingStar/RatingStar.spec.tsx
+++ src/components/Rating/RatingStar/RatingStar.spec.tsx
@@ -4,18 +4,18 @@ import Rating from '../Rating';
 import RatingStar from './RatingStar';
 
 describe('RatingStar component unit tests', () => {
-  const mockProps = {
-    id: 'test-id',
-    testId: 'test-rating-star',
-  };
-  const { getByTestId } = render(
-    <Rating>
-      <RatingStar {...mockProps} />
-    </Rating>,
-  );
-  it('should render RatingStar component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-rating-star')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-id',
+        testId: 'test-rating-star',
+    };
+    const { getByTestId } = render(
+        <Rating>
+            <RatingStar {...mockProps} />
+        </Rating>,
+    );
+    it('should render RatingStar component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-rating-star')).toBeInTheDocument();
+        });
     });
-  });
 });
