5a7993b1bf17e16a23b94669652da36b25a3edb0
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-04 17:46:42 +0330

    fix: modified with new vscode settings

--- src/components/Rating/RatingAdvanced/RatingAdvanced.spec.tsx
+++ src/components/Rating/RatingAdvanced/RatingAdvanced.spec.tsx
@@ -3,15 +3,15 @@ import { render, waitFor } from '@testing-library/react';
 import RatingAdvanced from './RatingAdvanced';
 
 describe('RatingAdvanced component', () => {
-  const mockProps = {
-    id: 'test-id',
-    percentFilled: 50,
-    testId: 'test-rating-advanced',
-  };
-  const { getByTestId } = render(<RatingAdvanced {...mockProps} />);
-  it('should render RatingAdvanced component with required props', () => {
-    waitFor(() => {
-      expect(getByTestId('test-rating-advanced')).toBeInTheDocument();
+    const mockProps = {
+        id: 'test-id',
+        percentFilled: 50,
+        testId: 'test-rating-advanced',
+    };
+    const { getByTestId } = render(<RatingAdvanced {...mockProps} />);
+    it('should render RatingAdvanced component with required props', () => {
+        waitFor(() => {
+            expect(getByTestId('test-rating-advanced')).toBeInTheDocument();
+        });
     });
-  });
 });
