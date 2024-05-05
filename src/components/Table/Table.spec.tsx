d90f0d6f7ccbc7839e682b09a8cc82bc936cc627
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-05 11:40:48 +0330

    fix: modified unit tests

--- src/components/Table/Table.spec.tsx
+++ src/components/Table/Table.spec.tsx
@@ -2,16 +2,16 @@ import { render, waitFor } from '@testing-library/react';
 
 import Table from './Table';
 
+const mockProps = {
+    id: 'test-table',
+    striped: true,
+    hoverable: true,
+    className: 'custom-table',
+    testId: 'test-table',
+};
 describe('Table component unit tests', () => {
-    const mockProps = {
-        id: 'test-table',
-        striped: true,
-        hoverable: true,
-        className: 'custom-table',
-        testId: 'test-table',
-    };
-    const { getByTestId } = render(<Table {...mockProps} />);
     it('should render Table component with required props', () => {
+        const { getByTestId } = render(<Table {...mockProps} />);
         waitFor(() => {
             const tableComponent = getByTestId('test-table');
             expect(tableComponent).toBeInTheDocument();
