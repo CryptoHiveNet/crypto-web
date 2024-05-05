424c43eaf6521444043f430ce893784cc9f81102
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-05 11:40:48 +0330

    fix: modified unit tests

--- src/components/Table/TableCell/TableCell.spec.tsx
+++ src/components/Table/TableCell/TableCell.spec.tsx
@@ -5,22 +5,22 @@ import TableBody from '../TableBody/TableBody';
 import TableRow from '../TableRow/TableRow';
 import TableCell from './TableCell';
 
+const mockProps = {
+    id: 'test-table-cell',
+    className: 'custom-table-cell',
+    testId: 'test-table-cell',
+};
 describe('TableCell component unit tests', () => {
-    const mockProps = {
-        id: 'test-table-cell',
-        className: 'custom-table-cell',
-        testId: 'test-table-cell',
-    };
-    const { getByTestId } = render(
-        <Table>
-            <TableBody>
-                <TableRow>
-                    <TableCell {...mockProps} />
-                </TableRow>
-            </TableBody>
-        </Table>,
-    );
     it('should render TableCell component with required props', () => {
+        const { getByTestId } = render(
+            <Table>
+                <TableBody>
+                    <TableRow>
+                        <TableCell {...mockProps} />
+                    </TableRow>
+                </TableBody>
+            </Table>,
+        );
         waitFor(() => {
             const tableCellComponent = getByTestId('test-table-cell');
             expect(tableCellComponent).toBeInTheDocument();
--- src/components/Table/TableHead/TableHead.spec.tsx
+++ src/components/Table/TableHead/TableHead.spec.tsx
@@ -3,18 +3,18 @@ import { render, waitFor } from '@testing-library/react';
 import Table from '../Table';
 import TableHead from './TableHead';
 
+const mockProps = {
+    id: 'test-table-head',
+    className: 'custom-table-head',
+    testId: 'test-table-head',
+};
 describe('TableHead component unit tests', () => {
-    const mockProps = {
-        id: 'test-table-head',
-        className: 'custom-table-head',
-        testId: 'test-table-head',
-    };
-    const { getByTestId } = render(
-        <Table>
-            <TableHead {...mockProps} />
-        </Table>,
-    );
     it('should render TableHead component with required props', () => {
+        const { getByTestId } = render(
+            <Table>
+                <TableHead {...mockProps} />
+            </Table>,
+        );
         waitFor(() => {
             const tableHeadComponent = getByTestId('test-table-head');
             expect(tableHeadComponent).toBeInTheDocument();
