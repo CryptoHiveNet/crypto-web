780cb3b80ffe1aea410929f8d7fef603fccf2cec
Amir <haghighi251@gmail.com>
2024-05-11 12:32:25 +0200

    refactor: update after reviewing the fix/pr41-splitting-part-12

--- src/components/Tabs/Tabs.spec.tsx
+++ src/components/Tabs/Tabs.spec.tsx
@@ -1,35 +1,50 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
 
-import Tabs from './Tabs';
-import TabsItem from './TabsItem/TabsItem';
+import Tabs from "./Tabs";
+import TabsItem from "./TabsItem/TabsItem";
 
-describe('Tabs component unit tests', () => {
-    const mockOnActiveTabChange = jest.fn();
-    const mockProps = {
-        id: 'test-tabs',
-        ariaLabel: 'Test Tabs',
-        className: 'tabs',
-        testId: 'test-tabs',
-        onActiveTabChange: mockOnActiveTabChange,
-    };
-    const { getByTestId, getByText } = render(
+const mockOnActiveTabChange = jest.fn();
+
+const mockProps = {
+  id: "test-tabs",
+  ariaLabel: "Test Tabs",
+  className: "tabs",
+  testId: "test-tabs",
+  onActiveTabChange: mockOnActiveTabChange,
+};
+
+describe("Tabs component unit tests", () => {
+  beforeEach(() => {
+    mockOnActiveTabChange.mockClear();
+  });
+
+  it("should render Tabs component with required props", () => {
+    render(
         <Tabs {...mockProps}>
-            <TabsItem title="Tab 1">Tab 1 Content</TabsItem>
-            <TabsItem title="Tab 2">Tab 2 Content</TabsItem>
-        </Tabs>,
-    );
-    it('should render Tabs component with required props', () => {
-        waitFor(() => {
-            const tabsComponent = getByTestId('test-tabs');
-            expect(tabsComponent).toBeInTheDocument();
-            expect(tabsComponent).toHaveAttribute('aria-label', 'Test Tabs');
-            expect(tabsComponent).toHaveClass('tabs');
-        });
+          <TabsItem title="Tab 1">Tab 1 Content</TabsItem>
+          <TabsItem title="Tab 2">Tab 2 Content</TabsItem>
+        </Tabs>
+      );
+
+      const tabsComponent = screen.getByTestId("test-tabs"); 
+
+      expect(tabsComponent).toBeInTheDocument();
+      expect(tabsComponent).toHaveAttribute("aria-label", "Test Tabs");
+      expect(tabsComponent).toHaveClass("tabs");
+  });
+
+  it("should fire onActiveTabChange callback when a tab is clicked", () => {
+    render(
+        <Tabs {...mockProps}>
+          <TabsItem title="Tab 1">Tab 1 Content</TabsItem>
+          <TabsItem title="Tab 2">Tab 2 Content</TabsItem>
+        </Tabs>
+      );
+
+    fireEvent.click(screen.getByText("Tab 2"));
+
+    waitFor(() => {
+      expect(mockOnActiveTabChange).toHaveBeenCalledWith("Tab 2");
     });
-    it('should fire onActiveTabChange callback when a tab is clicked', () => {
-        waitFor(() => {
-            fireEvent.click(getByText('Tab 2'));
-            expect(mockOnActiveTabChange).toHaveBeenCalledWith('Tab 2');
-        });
-    });
+  });
 });
