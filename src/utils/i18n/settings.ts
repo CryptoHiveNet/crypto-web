726386b30e98d7e5772d2375edf3f44a0ee40f1a
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-14 19:58:18 +0330

    feat: working on changes on first load problem

--- src/utils/i18n/settings.ts
+++ src/utils/i18n/settings.ts
@@ -5,9 +5,9 @@ export const cookieName = 'i18next';
 
 export function getOptions(lng = fallbackLng, ns = defaultNS) {
     return {
-        // debug: true,
+        debug: true,
         supportedLngs: languages,
-        // preload: languages,
+        preload: languages,
         fallbackLng,
         lng,
         fallbackNS: defaultNS,
