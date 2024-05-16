684f116d45b36b3c77050615cbff150c7a1c7865
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-16 18:59:11 +0330

    refactor: modified Accordion unit tests

--- src/shared/types/components/accordion.ts
+++ src/shared/types/components/accordion.ts
@@ -1,24 +1,25 @@
 import { ReactNode } from 'react';
 
 export type AccordionProps = {
-  id?: string;
-  collapseAll?: boolean;
-  className?: string;
-  children?: any;
-  testId?: string;
-  onClick?: () => void;
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
+    id?: string;
+    collapseAll?: boolean;
+    className?: string;
+    children?: any;
+    testId?: string;
+    onClick?: () => void;
+    onMouseEnter?: () => void;
+    onMouseLeave?: () => void;
 };
 export type AccordionItemProps = {
-  id?: string;
-  className?: string;
-  titleClassName?: string;
-  contentClassName?: string;
-  title: string;
-  content: ReactNode;
-  testId?: string;
-  onClick?: () => void;
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
+    id?: string;
+    className?: string;
+    titleClassName?: string;
+    contentClassName?: string;
+    title: string;
+    content: ReactNode;
+    titleTestId?: string;
+    contentTestId?: string;
+    onClick?: () => void;
+    onMouseEnter?: () => void;
+    onMouseLeave?: () => void;
 };
