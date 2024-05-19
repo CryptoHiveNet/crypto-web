2a0c7a6bec7299e80a3b62cba41aebe64293c13d
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-19 19:16:13 +0330

    refactor: fixed the Card unit test

--- src/components/Card/Card.spec.tsx
+++ src/components/Card/Card.spec.tsx
@@ -1,29 +1,79 @@
-import {
-    img_alt,
-    img_src,
-    test_children_text,
-} from '@/types/__mocks__/textMock';
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Card from './Card';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const props = {
+    id: 'test-id',
+    href: 'https://example.com',
+    imgAlt: 'Example Image',
+    imgSrc: 'https://example.com/image.jpg',
+    horizontal: true,
+    className: 'custom-class',
+    testId: 'my-card',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+const test_children_text = 'Test Children';
 describe('Card component unit tests', () => {
-    const { getByText, getByAltText } = render(
-        <Card
-            imgSrc={img_src}
-            imgAlt={img_alt}
-        >
-            {test_children_text}
-        </Card>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('renders a card with the provided content', () => {
-        expect(getByText(test_children_text)).toBeInTheDocument();
+        render(<Card {...props}>{test_children_text}</Card>);
+        expect(screen.getByText(test_children_text)).toBeInTheDocument();
     });
 
     it('renders a card with an image', () => {
-        waitFor(() => {
-            expect(getByAltText(img_alt)).toBeInTheDocument();
-            expect(getByAltText(img_alt)).toHaveAttribute('src', img_src);
-        });
+        render(<Card {...props} />);
+        const image = screen.getByAltText('Example Image');
+        expect(image).toBeInTheDocument();
+        expect(image).toHaveAttribute('src', 'https://example.com/image.jpg');
+    });
+
+    it('applies additional class name correctly', () => {
+        render(<Card {...props} />);
+        const cardComponent = screen.getByTestId(props.testId);
+        expect(cardComponent).toHaveClass(props.className);
+    });
+
+    it('calls onClick function when card is clicked', () => {
+        render(<Card {...props} />);
+        const cardComponent = screen.getByTestId(props.testId);
+        fireEvent.click(cardComponent);
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should call onMouseEnter when mouse enters', () => {
+        render(<Card {...props} />);
+        const cardComponent = screen.getByTestId(props.testId);
+        fireEvent.mouseEnter(cardComponent);
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should call onMouseLeave when mouse leaves', () => {
+        render(<Card {...props} />);
+        const cardComponent = screen.getByTestId(props.testId);
+        fireEvent.mouseLeave(cardComponent);
+        expect(onMouseLeaveMock).toHaveBeenCalled();
+    });
+
+    it('should pass other props down correctly', () => {
+        const otherProps = { 'aria-label': 'card' };
+        render(
+            <Card
+                {...props}
+                {...otherProps}
+            >
+                {test_children_text}
+            </Card>,
+        );
+        const cardComponent = screen.getByTestId(props.testId);
+        expect(cardComponent).toHaveAttribute('aria-label', 'card');
     });
 });
