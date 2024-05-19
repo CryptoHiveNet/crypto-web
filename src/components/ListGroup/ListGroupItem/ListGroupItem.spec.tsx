a217d7fb907ec5c724a9903b5c15cad5e56ea516
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-19 21:12:47 +0330

    refactor: fixed the ListGroup unit test

--- src/components/ListGroup/ListGroupItem/ListGroupItem.spec.tsx
+++ src/components/ListGroup/ListGroupItem/ListGroupItem.spec.tsx
@@ -1,32 +1,45 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import ListGroupItem from './ListGroupItem';
 
+const onClickMock = jest.fn();
+const onMouseEnter = jest.fn();
+const onMouseLeave = jest.fn();
+const mockProps = {
+    id: 'test-id',
+    testId: 'test-list-group-item',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnter,
+    onMouseLeave: onMouseLeave,
+    className: 'test-class',
+};
+const children = 'Test Children';
+
 describe('ListGroupItem component unit tests', () => {
-    const onClickMock = jest.fn();
-    const mockProps = {
-        id: 'test-id',
-        testId: 'test-list-group-item',
-        onClick: onClickMock,
-    };
-    const mockChildren = <div>Test Children</div>;
-    const { getByTestId, getByText } = render(
-        <ListGroupItem {...mockProps}>{mockChildren}</ListGroupItem>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
     it('should render ListGroupItem component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-list-group-item')).toBeInTheDocument();
-        });
+        render(<ListGroupItem {...mockProps}>{children}</ListGroupItem>);
+        expect(screen.getByTestId('test-list-group-item')).toBeInTheDocument();
     });
+
     it('should handle onClick event', () => {
-        waitFor(() => {
-            fireEvent.click(getByTestId('test-list-group-item'));
-            expect(onClickMock).toHaveBeenCalled();
-        });
+        render(<ListGroupItem {...mockProps}>{children}</ListGroupItem>);
+        fireEvent.click(screen.getByTestId('test-list-group-item'));
+        expect(onClickMock).toHaveBeenCalled();
     });
+
     it('should render children', () => {
-        waitFor(() => {
-            expect(getByText('Test Children')).toBeInTheDocument();
-        });
+        render(<ListGroupItem {...mockProps}>{children}</ListGroupItem>);
+        expect(screen.getByText('Test Children')).toBeInTheDocument();
+    });
+
+    it('should call onMouseEnter and onMouseLeave events', () => {
+        render(<ListGroupItem {...mockProps}>{children}</ListGroupItem>);
+        fireEvent.mouseEnter(screen.getByTestId('test-list-group-item'));
+        fireEvent.mouseLeave(screen.getByTestId('test-list-group-item'));
+        expect(mockProps.onMouseEnter).toHaveBeenCalled();
+        expect(mockProps.onMouseLeave).toHaveBeenCalled();
     });
 });
