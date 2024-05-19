20254041ffe3b86ef101acaf352d602521e23b6a
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-19 20:59:29 +0330

    refactor: fixed the Footer unit test

--- src/components/Footer/FooterDivider/FooterDivider.spec.tsx
+++ src/components/Footer/FooterDivider/FooterDivider.spec.tsx
@@ -1,24 +1,46 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import FooterDivider from './FooterDivider';
 
+const onClickMock = jest.fn();
+const onMouseEnter = jest.fn();
+const onMouseLeave = jest.fn();
+const mockProps = {
+    id: 'test-id',
+    testId: 'test-footer-divider',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnter,
+    onMouseLeave: onMouseLeave,
+    className: 'test-class',
+};
+
 describe('FooterDivider component unit tests', () => {
-    const onClickMock = jest.fn();
-    const mockProps = {
-        id: 'test-id',
-        testId: 'test-footer-divider',
-        onClick: onClickMock,
-    };
-    const { getByTestId } = render(<FooterDivider {...mockProps} />);
-    it('should render FooterDivider component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-footer-divider')).toBeInTheDocument();
-        });
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
+    it('should render FooterDivider component with required props', () => {
+        render(<FooterDivider {...mockProps} />);
+        expect(screen.getByTestId('test-footer-divider')).toBeInTheDocument();
+    });
+
     it('should handle onClick event', () => {
-        waitFor(() => {
-            fireEvent.click(getByTestId('test-footer-divider'));
-            expect(onClickMock).toHaveBeenCalled();
-        });
+        render(<FooterDivider {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-footer-divider'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should apply className prop', () => {
+        render(<FooterDivider {...mockProps} />);
+        expect(screen.getByTestId('test-footer-divider')).toHaveClass(
+            'test-class',
+        );
+    });
+
+    it('should call onMouseEnter and onMouseLeave events', () => {
+        render(<FooterDivider {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-footer-divider'));
+        fireEvent.mouseLeave(screen.getByTestId('test-footer-divider'));
+        expect(mockProps.onMouseEnter).toHaveBeenCalled();
+        expect(mockProps.onMouseLeave).toHaveBeenCalled();
     });
 });
