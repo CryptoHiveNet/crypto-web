43c42b5618af934d129e48ff3fd9445b78070830
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-19 20:20:23 +0330

    refactor: fixed the Dropdown unit test

--- src/components/Dropdown/Dropdown.spec.tsx
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -1,25 +1,49 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Dropdown from './Dropdown';
 
-describe('Dropdown', () => {
-    const label = 'Options';
-    const size = 'large';
-    const { getByText, container } = render(
-        <Dropdown
-            label={label}
-            size={size}
-        />,
-    );
-    it('renders a dropdown with the provided label', () => {
-        waitFor(() => {
-            expect(getByText(label)).toBeInTheDocument();
-        });
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-id',
+    label: 'Options',
+    dismissOnClick: true,
+    inline: false,
+    size: 'lg',
+    placement: 'bottom',
+    className: 'custom-class',
+    testId: 'flowbite-dropdown-target',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
+describe('Dropdown component unit tests', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
 
-    it('renders a dropdown with the provided size', () => {
-        waitFor(() => {
-            expect(container.firstChild).toHaveClass(`fb-dropdown-${size}`);
-        });
+    it('renders a dropdown with the provided label', () => {
+        render(
+            <Dropdown
+                {...mockProps}
+                renderTrigger={() => <button>Trigger</button>}
+            />,
+        );
+        expect(screen.getByText('Trigger')).toBeInTheDocument();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(<Dropdown {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId(mockProps.testId));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(<Dropdown {...mockProps} />);
+        fireEvent.mouseLeave(screen.getByTestId(mockProps.testId));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
