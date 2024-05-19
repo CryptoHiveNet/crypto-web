96c77a4b3119656da2af4c7e83b15061f44b2e10
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-19 20:59:29 +0330

    refactor: fixed the Footer unit test

--- src/components/Footer/FooterLinkGroup/FooterLinkGroup.spec.tsx
+++ src/components/Footer/FooterLinkGroup/FooterLinkGroup.spec.tsx
@@ -1,39 +1,62 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import FooterLinkGroup from './FooterLinkGroup';
 
+const onClickMock = jest.fn();
+const onMouseEnter = jest.fn();
+const onMouseLeave = jest.fn();
+const mockProps = {
+    id: 'test-id',
+    col: 3,
+    testId: 'test-footer-link-group',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnter,
+    onMouseLeave: onMouseLeave,
+    className: 'test-class',
+};
+const mockChildren = (
+    <>
+        <div>Group 1</div>
+        <div>Group 2</div>
+    </>
+);
 describe('FooterLinkGroup component unit tests', () => {
-    const onClickMock = jest.fn();
-    const mockProps = {
-        id: 'test-id',
-        col: 3,
-        testId: 'test-footer-link-group',
-        onClick: onClickMock,
-    };
-    const mockChildren = (
-        <>
-            <div>Group 1</div>
-            <div>Group 2</div>
-        </>
-    );
-    const { getByTestId, getByText } = render(
-        <FooterLinkGroup {...mockProps}>{mockChildren}</FooterLinkGroup>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
     it('should render FooterLinkGroup component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-footer-link-group')).toBeInTheDocument();
-        });
+        render(<FooterLinkGroup {...mockProps} />);
+        expect(
+            screen.getByTestId('test-footer-link-group'),
+        ).toBeInTheDocument();
     });
+
     it('should handle onClick event', () => {
-        waitFor(() => {
-            fireEvent.click(getByTestId('test-footer-link-group'));
-            expect(onClickMock).toHaveBeenCalled();
-        });
+        render(<FooterLinkGroup {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-footer-link-group'));
+        expect(onClickMock).toHaveBeenCalled();
     });
+
     it('should render children', () => {
-        waitFor(() => {
-            expect(getByText('Group 1')).toBeInTheDocument();
-            expect(getByText('Group 2')).toBeInTheDocument();
-        });
+        render(
+            <FooterLinkGroup {...mockProps}>{mockChildren}</FooterLinkGroup>,
+        );
+        expect(screen.getByText('Group 1')).toBeInTheDocument();
+        expect(screen.getByText('Group 2')).toBeInTheDocument();
+    });
+
+    it('should apply className prop', () => {
+        render(<FooterLinkGroup {...mockProps} />);
+        expect(screen.getByTestId('test-footer-link-group')).toHaveClass(
+            'test-class',
+        );
+    });
+
+    it('should call onMouseEnter and onMouseLeave events', () => {
+        render(<FooterLinkGroup {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-footer-link-group'));
+        fireEvent.mouseLeave(screen.getByTestId('test-footer-link-group'));
+        expect(mockProps.onMouseEnter).toHaveBeenCalled();
+        expect(mockProps.onMouseLeave).toHaveBeenCalled();
     });
 });
