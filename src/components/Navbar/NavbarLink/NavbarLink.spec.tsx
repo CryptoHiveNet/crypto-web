98421249ce5dae52db7b639a3bd8c2a43cacae32
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 11:40:30 +0330

    refactor: fixed the Navbar unit test

--- src/components/Navbar/NavbarLink/NavbarLink.spec.tsx
+++ src/components/Navbar/NavbarLink/NavbarLink.spec.tsx
@@ -1,35 +1,70 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Navbar from '../Navbar';
 import NavbarLink from './NavbarLink';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-id',
+    className: 'custom-navbar-link',
+    href: '/test',
+    testId: 'test-navbar-link',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
 describe('NavbarLink component unit tests', () => {
-    const onClickMock = jest.fn();
-    const mockProps = {
-        id: 'test-id',
-        href: '#',
-        testId: 'test-navbar-link',
-        onClick: onClickMock,
-    };
-    const { getByTestId } = render(
-        <Navbar>
-            <NavbarLink {...mockProps}>Link</NavbarLink>
-        </Navbar>,
-    );
-    it('should render NavbarLink component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-navbar-link')).toBeInTheDocument();
-            expect(getByTestId('test-navbar-link')).toHaveAttribute(
-                'href',
-                '#',
-            );
-            expect(getByTestId('test-navbar-link')).toHaveTextContent('Link');
-        });
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
+
+    it('should render NavbarLink component with required props', () => {
+        render(
+            <Navbar>
+                <NavbarLink {...mockProps}>Test Link</NavbarLink>
+            </Navbar>,
+        );
+        const navbarLinkComponent = screen.getByTestId('test-navbar-link');
+        expect(navbarLinkComponent).toBeInTheDocument();
+        expect(navbarLinkComponent).toHaveClass('custom-navbar-link');
+        expect(navbarLinkComponent).toHaveAttribute('href', '/test');
+        expect(navbarLinkComponent).toHaveTextContent('Test Link');
+    });
+
     it('should handle onClick event', () => {
-        waitFor(() => {
-            fireEvent.click(getByTestId('test-navbar-link'));
-            expect(onClickMock).toHaveBeenCalled();
-        });
+        render(
+            <Navbar>
+                <NavbarLink {...mockProps}>Test Link</NavbarLink>
+            </Navbar>,
+        );
+        const navbarLinkComponent = screen.getByTestId('test-navbar-link');
+        fireEvent.click(navbarLinkComponent);
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(
+            <Navbar>
+                <NavbarLink {...mockProps}>Test Link</NavbarLink>
+            </Navbar>,
+        );
+        const navbarLinkComponent = screen.getByTestId('test-navbar-link');
+        fireEvent.mouseEnter(navbarLinkComponent);
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(
+            <Navbar>
+                <NavbarLink {...mockProps}>Test Link</NavbarLink>
+            </Navbar>,
+        );
+        const navbarLinkComponent = screen.getByTestId('test-navbar-link');
+        fireEvent.mouseLeave(navbarLinkComponent);
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
