a30906fff10f1b68060e1c24724fd02dcc166cc2
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 19:56:59 +0330

    refactor: fixed the Toast unit test

--- src/components/Toast/ToastToggle/ToastToggle.spec.tsx
+++ src/components/Toast/ToastToggle/ToastToggle.spec.tsx
@@ -1,34 +1,77 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Toast from '../Toast';
 import ToastToggle from './ToastToggle';
 
+const onDismissMock = jest.fn();
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-toast-toggle',
+    className: 'custom-toast-toggle',
+    testId: 'test-toast-toggle',
+    onDismiss: onDismissMock,
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
 describe('ToastToggle component unit tests', () => {
-    const mockOnClick = jest.fn();
-    const mockProps = {
-        id: 'test-toast-toggle',
-        className: 'custom-toast-toggle',
-        testId: 'test-toast-toggle',
-        onDismiss: jest.fn(),
-        onClick: mockOnClick,
-    };
-    const { getByTestId } = render(
-        <Toast>
-            <ToastToggle {...mockProps} />
-        </Toast>,
-    );
-    it('should render ToastToggle component with required props', () => {
-        waitFor(() => {
-            const toastToggleComponent = getByTestId('test-toast-toggle');
-            expect(toastToggleComponent).toBeInTheDocument();
-            expect(toastToggleComponent).toHaveClass('custom-toast-toggle');
-        });
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
+
+    it('should render ToastToggle component with required props', () => {
+        render(
+            <Toast>
+                <ToastToggle {...mockProps} />
+            </Toast>,
+        );
+        const toastToggleComponent = screen.getByTestId('test-toast-toggle');
+        expect(toastToggleComponent).toBeInTheDocument();
+        expect(toastToggleComponent).toHaveClass('custom-toast-toggle');
+        expect(toastToggleComponent).toHaveAttribute('id', 'test-toast-toggle');
+    });
+
+    it('should handle onDismiss event', () => {
+        render(
+            <Toast>
+                <ToastToggle {...mockProps} />
+            </Toast>,
+        );
+        fireEvent.click(screen.getByTestId('test-toast-toggle'));
+        expect(onDismissMock).toHaveBeenCalled();
+    });
+
     it('should handle onClick event', () => {
-        waitFor(() => {
-            const toastToggleComponent = getByTestId('test-toast-toggle');
-            fireEvent.click(toastToggleComponent);
-            expect(mockOnClick).toHaveBeenCalledTimes(1);
-        });
+        render(
+            <Toast>
+                <ToastToggle {...mockProps} />
+            </Toast>,
+        );
+        fireEvent.click(screen.getByTestId('test-toast-toggle'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(
+            <Toast>
+                <ToastToggle {...mockProps} />
+            </Toast>,
+        );
+        fireEvent.mouseEnter(screen.getByTestId('test-toast-toggle'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(
+            <Toast>
+                <ToastToggle {...mockProps} />
+            </Toast>,
+        );
+        fireEvent.mouseLeave(screen.getByTestId('test-toast-toggle'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
