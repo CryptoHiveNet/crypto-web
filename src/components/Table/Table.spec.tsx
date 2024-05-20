11a256e16ad2db6a4101a75bb41de2a651c7524e
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 12:37:04 +0330

    refactor: fixed the Table unit test

--- src/components/Table/Table.spec.tsx
+++ src/components/Table/Table.spec.tsx
@@ -1,6 +1,10 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Table from './Table';
+
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
 
 const mockProps = {
     id: 'test-table',
@@ -8,14 +12,38 @@ const mockProps = {
     hoverable: true,
     className: 'custom-table',
     testId: 'test-table',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
 };
+
 describe('Table component unit tests', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render Table component with required props', () => {
         render(<Table {...mockProps} />);
-        waitFor(() => {
-            const tableComponent = screen.getByTestId('test-table');
-            expect(tableComponent).toBeInTheDocument();
-            expect(tableComponent).toHaveClass('custom-table');
-        });
+        const tableComponent = screen.getByTestId('test-table');
+        expect(tableComponent).toBeInTheDocument();
+        expect(tableComponent).toHaveClass('custom-table');
+    });
+
+    it('should handle onClick event', () => {
+        render(<Table {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-table'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(<Table {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-table'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(<Table {...mockProps} />);
+        fireEvent.mouseLeave(screen.getByTestId('test-table'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
