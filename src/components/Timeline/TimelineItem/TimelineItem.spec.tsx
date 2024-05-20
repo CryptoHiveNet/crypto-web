a4f66569062fa304c6c3402436bb7548e931bdba
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 19:53:43 +0330

    refactor: fixed the Timeline unit test

--- src/components/Timeline/TimelineItem/TimelineItem.spec.tsx
+++ src/components/Timeline/TimelineItem/TimelineItem.spec.tsx
@@ -1,25 +1,79 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Timeline from '../Timeline';
 import TimelineItem from './TimelineItem';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-timeline-item',
+    className: 'custom-timeline-item',
+    testId: 'test-timeline-item',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+    children: <div>Test Content</div>,
+};
+
 describe('TimelineItem component unit tests', () => {
-    const mockProps = {
-        id: 'test-timeline-item',
-        className: 'timeline-item',
-        testId: 'test-timeline-item',
-        onClick: jest.fn(),
-    };
-    const { getByTestId } = render(
-        <Timeline>
-            <TimelineItem {...mockProps} />
-        </Timeline>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render TimelineItem component with required props', () => {
-        waitFor(() => {
-            const timelineItem = getByTestId('test-timeline-item');
-            expect(timelineItem).toBeInTheDocument();
-            expect(timelineItem).toHaveClass('timeline-item');
-        });
+        render(
+            <Timeline>
+                <TimelineItem {...mockProps} />
+            </Timeline>,
+        );
+        const timelineItemComponent = screen.getByTestId('test-timeline-item');
+        expect(timelineItemComponent).toBeInTheDocument();
+        expect(timelineItemComponent).toHaveClass('custom-timeline-item');
+        expect(timelineItemComponent).toHaveAttribute(
+            'id',
+            'test-timeline-item',
+        );
+    });
+
+    it('should handle onClick event', () => {
+        render(
+            <Timeline>
+                <TimelineItem {...mockProps} />
+            </Timeline>,
+        );
+        fireEvent.click(screen.getByTestId('test-timeline-item'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(
+            <Timeline>
+                <TimelineItem {...mockProps} />
+            </Timeline>,
+        );
+        fireEvent.mouseEnter(screen.getByTestId('test-timeline-item'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(
+            <Timeline>
+                <TimelineItem {...mockProps} />
+            </Timeline>,
+        );
+        fireEvent.mouseLeave(screen.getByTestId('test-timeline-item'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
+    });
+
+    it('should render children inside the TimelineItem component', () => {
+        render(
+            <Timeline>
+                <TimelineItem {...mockProps} />
+            </Timeline>,
+        );
+        const childElement = screen.getByText('Test Content');
+        expect(childElement).toBeInTheDocument();
     });
 });
--- src/components/Timeline/TimelineTime/TimelineTime.spec.tsx
+++ src/components/Timeline/TimelineTime/TimelineTime.spec.tsx
@@ -1,33 +1,110 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Timeline from '../Timeline';
 import TimelineContent from '../TimelineContent/TimelineContent';
 import TimelineItem from '../TimelineItem/TimelineItem';
 import TimelineTime from './TimelineTime';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-timeline-time',
+    className: 'custom-timeline-time',
+    testId: 'test-timeline-time',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+    children: 'Timeline Time',
+};
+
 describe('TimelineTime component unit tests', () => {
-    const mockProps = {
-        id: 'test-timeline-time',
-        className: 'timeline-time',
-        testId: 'test-timeline-time',
-        onClick: jest.fn(),
-    };
-    const { getByTestId } = render(
-        <Timeline>
-            <TimelineItem>
-                <TimelineContent>
-                    <TimelineTime {...mockProps}>
-                        Test Timeline Time
-                    </TimelineTime>
-                </TimelineContent>
-            </TimelineItem>
-        </Timeline>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render TimelineTime component with required props', () => {
-        waitFor(() => {
-            const timelineTime = getByTestId('test-timeline-time');
-            expect(timelineTime).toBeInTheDocument();
-            expect(timelineTime).toHaveTextContent('Test Timeline Time');
-        });
+        render(
+            <Timeline>
+                <TimelineItem>
+                    <TimelineContent>
+                        <TimelineTime {...mockProps}>
+                            Test Timeline Time
+                        </TimelineTime>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>,
+        );
+        const timelineTimeComponent = screen.getByTestId('test-timeline-time');
+        expect(timelineTimeComponent).toBeInTheDocument();
+        expect(timelineTimeComponent).toHaveClass('custom-timeline-time');
+        expect(timelineTimeComponent).toHaveAttribute(
+            'id',
+            'test-timeline-time',
+        );
+    });
+
+    it('should handle onClick event', () => {
+        render(
+            <Timeline>
+                <TimelineItem>
+                    <TimelineContent>
+                        <TimelineTime {...mockProps}>
+                            Test Timeline Time
+                        </TimelineTime>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>,
+        );
+        fireEvent.click(screen.getByTestId('test-timeline-time'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(
+            <Timeline>
+                <TimelineItem>
+                    <TimelineContent>
+                        <TimelineTime {...mockProps}>
+                            Test Timeline Time
+                        </TimelineTime>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>,
+        );
+        fireEvent.mouseEnter(screen.getByTestId('test-timeline-time'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(
+            <Timeline>
+                <TimelineItem>
+                    <TimelineContent>
+                        <TimelineTime {...mockProps}>
+                            Test Timeline Time
+                        </TimelineTime>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>,
+        );
+        fireEvent.mouseLeave(screen.getByTestId('test-timeline-time'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
+    });
+
+    it('should render children inside the TimelineTime component', () => {
+        render(
+            <Timeline>
+                <TimelineItem>
+                    <TimelineContent>
+                        <TimelineTime {...mockProps}>
+                            Test Timeline Time
+                        </TimelineTime>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>,
+        );
+        expect(screen.getByText('Test Timeline Time')).toBeInTheDocument();
     });
 });
