aba744a3aaea3a52f1493cadd935884a98800098
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 12:22:26 +0330

    refactor: fixed the Sidebar unit test

--- src/components/Sidebar/SidebarLogo/SidebarLogo.spec.tsx
+++ src/components/Sidebar/SidebarLogo/SidebarLogo.spec.tsx
@@ -1,24 +1,69 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Sidebar from '../Sidebar';
 import SidebarLogo from './SidebarLogo';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-sidebar-logo',
+    href: '#',
+    img: '/logo.png',
+    imgAlt: 'Logo',
+    className: 'custom-sidebar-logo',
+    testId: 'test-sidebar-logo',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
 describe('SidebarLogo component unit tests', () => {
-    const mockProps = {
-        id: 'test-logo',
-        href: '/',
-        img: '/path/to/image.png',
-        imgAlt: 'Logo',
-        testId: 'test-sidebar-logo',
-    };
-    const { getByTestId } = render(
-        <Sidebar>
-            <SidebarLogo {...mockProps} />
-        </Sidebar>,
-    );
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render SidebarLogo component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-sidebar-logo')).toBeInTheDocument();
-        });
+        render(
+            <Sidebar>
+                <SidebarLogo {...mockProps} />
+            </Sidebar>,
+        );
+        const sidebarLogoComponent = screen.getByTestId('test-sidebar-logo');
+        expect(sidebarLogoComponent).toBeInTheDocument();
+        expect(sidebarLogoComponent).toHaveClass('custom-sidebar-logo');
+        expect(screen.getByAltText('Logo')).toBeInTheDocument();
+        // Add additional assertions based on the specific behavior of the SidebarLogo component
+    });
+
+    it('should handle onClick event', () => {
+        render(
+            <Sidebar>
+                <SidebarLogo {...mockProps} />
+            </Sidebar>,
+        );
+        fireEvent.click(screen.getByTestId('test-sidebar-logo'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(
+            <Sidebar>
+                <SidebarLogo {...mockProps} />
+            </Sidebar>,
+        );
+        fireEvent.mouseEnter(screen.getByTestId('test-sidebar-logo'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(
+            <Sidebar>
+                <SidebarLogo {...mockProps} />
+            </Sidebar>,
+        );
+        fireEvent.mouseLeave(screen.getByTestId('test-sidebar-logo'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
