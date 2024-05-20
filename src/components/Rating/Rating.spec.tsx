4297ae1b28f4757f1b55e7f13fe71dd6f4a4f82d
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 12:04:35 +0330

    refactor: fixed the Rating unit test

--- src/components/Rating/Rating.spec.tsx
+++ src/components/Rating/Rating.spec.tsx
@@ -1,17 +1,48 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Rating from './Rating';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-rating',
+    className: 'custom-rating',
+    size: 'md',
+    testId: 'test-rating',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
 describe('Rating component unit tests', () => {
-    const mockProps = {
-        id: 'test-id',
-        size: 'medium',
-        testId: 'test-rating',
-    };
-    const { getByTestId } = render(<Rating {...mockProps} />);
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render Rating component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-rating')).toBeInTheDocument();
-        });
+        render(<Rating {...mockProps} />);
+        const ratingComponent = screen.getByTestId('test-rating');
+        expect(ratingComponent).toBeInTheDocument();
+        expect(ratingComponent).toHaveClass('custom-rating');
+    });
+
+    it('should handle onClick event', () => {
+        render(<Rating {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-rating'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(<Rating {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-rating'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(<Rating {...mockProps} />);
+        fireEvent.mouseLeave(screen.getByTestId('test-rating'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
