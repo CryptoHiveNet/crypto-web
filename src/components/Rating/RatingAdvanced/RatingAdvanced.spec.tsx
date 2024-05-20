49bab198fbc99619391180687cda048da9a4f501
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 12:04:35 +0330

    refactor: fixed the Rating unit test

--- src/components/Rating/RatingAdvanced/RatingAdvanced.spec.tsx
+++ src/components/Rating/RatingAdvanced/RatingAdvanced.spec.tsx
@@ -1,17 +1,50 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import RatingAdvanced from './RatingAdvanced';
 
-describe('RatingAdvanced component', () => {
-    const mockProps = {
-        id: 'test-id',
-        percentFilled: 50,
-        testId: 'test-rating-advanced',
-    };
-    const { getByTestId } = render(<RatingAdvanced {...mockProps} />);
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-rating-advanced',
+    percentFilled: 80,
+    className: 'custom-rating-advanced',
+    testId: 'test-rating-advanced',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+};
+
+describe('RatingAdvanced component unit tests', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render RatingAdvanced component with required props', () => {
-        waitFor(() => {
-            expect(getByTestId('test-rating-advanced')).toBeInTheDocument();
-        });
+        render(<RatingAdvanced {...mockProps} />);
+        const ratingAdvancedComponent = screen.getByTestId(
+            'test-rating-advanced',
+        );
+        expect(ratingAdvancedComponent).toBeInTheDocument();
+        expect(ratingAdvancedComponent).toHaveClass('custom-rating-advanced');
+    });
+
+    it('should handle onClick event', () => {
+        render(<RatingAdvanced {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-rating-advanced'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(<RatingAdvanced {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-rating-advanced'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(<RatingAdvanced {...mockProps} />);
+        fireEvent.mouseLeave(screen.getByTestId('test-rating-advanced'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
     });
 });
