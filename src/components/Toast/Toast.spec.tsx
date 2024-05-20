778809ddda26b2ca1dd9447cc0f068310977ea6b
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-20 19:56:59 +0330

    refactor: fixed the Toast unit test

--- src/components/Toast/Toast.spec.tsx
+++ src/components/Toast/Toast.spec.tsx
@@ -1,20 +1,55 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Toast from './Toast';
 
+const onClickMock = jest.fn();
+const onMouseEnterMock = jest.fn();
+const onMouseLeaveMock = jest.fn();
+
+const mockProps = {
+    id: 'test-toast',
+    className: 'custom-toast',
+    testId: 'test-toast',
+    onClick: onClickMock,
+    onMouseEnter: onMouseEnterMock,
+    onMouseLeave: onMouseLeaveMock,
+    children: <div>Test Toast Message</div>,
+};
+
 describe('Toast component unit tests', () => {
-    const mockProps = {
-        id: 'test-toast',
-        className: 'custom-toast',
-        testId: 'test-toast',
-    };
-    const { getByTestId } = render(<Toast {...mockProps}>Test message</Toast>);
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render Toast component with required props', () => {
-        waitFor(() => {
-            const toastComponent = getByTestId('test-toast');
-            expect(toastComponent).toBeInTheDocument();
-            expect(toastComponent).toHaveTextContent('Test message');
-            expect(toastComponent).toHaveClass('custom-toast');
-        });
+        render(<Toast {...mockProps} />);
+        const toastComponent = screen.getByTestId('test-toast');
+        expect(toastComponent).toBeInTheDocument();
+        expect(toastComponent).toHaveClass('custom-toast');
+        expect(toastComponent).toHaveAttribute('id', 'test-toast');
+    });
+
+    it('should handle onClick event', () => {
+        render(<Toast {...mockProps} />);
+        fireEvent.click(screen.getByTestId('test-toast'));
+        expect(onClickMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseEnter event', () => {
+        render(<Toast {...mockProps} />);
+        fireEvent.mouseEnter(screen.getByTestId('test-toast'));
+        expect(onMouseEnterMock).toHaveBeenCalled();
+    });
+
+    it('should handle onMouseLeave event', () => {
+        render(<Toast {...mockProps} />);
+        fireEvent.mouseLeave(screen.getByTestId('test-toast'));
+        expect(onMouseLeaveMock).toHaveBeenCalled();
+    });
+
+    it('should render children inside the Toast component', () => {
+        render(<Toast {...mockProps} />);
+        const childElement = screen.getByText('Test Toast Message');
+        expect(childElement).toBeInTheDocument();
     });
 });
