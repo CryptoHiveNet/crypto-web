3a86910606a501522fe3fb486475279ed6b48f80
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-21 19:20:27 +0330

    fix: solved react-query import issue

--- src/modules/user/hooks/useRegisterUser.ts
+++ src/modules/user/hooks/useRegisterUser.ts
@@ -1,51 +1,60 @@
-"use client"
-import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
+'use client';
+import { useState } from 'react';
 
-import { Address } from "@/types/shared/types/user/Address";
-import { RegisterUserRequest } from "@/types/packages/api/contexts/user/application/types/RegisterUserRequest";
-import { NoticeMessage, Severity } from "@/types/shared/types/components/NoticeMessage";
-import { REGISTER } from "../../shared/constants/apiRoutes";
-import { HttpError } from "@/types/shared/infrastructures/http/HttpError";
-import { axiosClient } from "@/types/shared/infrastructures/http/AxiosClient";
-import HttpStatus from "@/types/shared/infrastructures/http/HttpStatus";
-import { UnhandledBusinessException } from "@/types/shared/Errors/UnhandledBusinessException";
-import { useMutationWithError } from "@/types/shared/hooks/useMutationWithError";
-import { addUser } from "../../shared/services/user/register/addUser";
+import { useTranslation } from 'react-i18next';
+
+import { RegisterUserRequest } from '@/types/packages/api/contexts/user/application/types/RegisterUserRequest';
+import { UnhandledBusinessException } from '@/types/shared/Errors/UnhandledBusinessException';
+import { useMutationWithError } from '@/types/shared/hooks/useMutationWithError';
+import { axiosClient } from '@/types/shared/infrastructures/http/AxiosClient';
+import { HttpError } from '@/types/shared/infrastructures/http/HttpError';
+import HttpStatus from '@/types/shared/infrastructures/http/HttpStatus';
+import {
+    NoticeMessage,
+    Severity,
+} from '@/types/shared/types/components/NoticeMessage';
+import { Address } from '@/types/shared/types/user/Address';
+import { useMutation } from '@tanstack/react-query';
+
+import { REGISTER } from '../../shared/constants/apiRoutes';
+import { addUser } from '../../shared/services/user/register/addUser';
 
 export type OnInvalidAddressErrorProps = {
-  registerUserRequest: RegisterUserRequest;
-  suggestedAddress?: Address;
+    registerUserRequest: RegisterUserRequest;
+    suggestedAddress?: Address;
 };
 
 export const useRegistration = () => {
-  const { t } = useTranslation();
-  const [errorMessages, setErrorMessages] = useState([] as NoticeMessage[]);
-  const [invalidAddressError, setInvalidAddressError] = useState<
-    OnInvalidAddressErrorProps | undefined
-  >();
+    const { t } = useTranslation();
+    const [errorMessages, setErrorMessages] = useState([] as NoticeMessage[]);
+    const [invalidAddressError, setInvalidAddressError] = useState<
+        OnInvalidAddressErrorProps | undefined
+    >();
 
-  const {mutate: registerMutation, isPending, isSuccess } = useMutationWithError(addUser, {
-    onSuccess: async () => {
-      // ToDo: This should handle the success part after the user registration.
-      // Force login and redirect to the user cPanel should be done here.
-    },
-    onError: async (error: unknown) => {
-      // ToDo: Update the errorMessages state regarding the error.
-    }
-  });
+    const {
+        mutate: registerMutation,
+        isPending,
+        isSuccess,
+    } = useMutationWithError(addUser, {
+        onSuccess: async () => {
+            // ToDo: This should handle the success part after the user registration.
+            // Force login and redirect to the user cPanel should be done here.
+        },
+        onError: async (error: unknown) => {
+            // ToDo: Update the errorMessages state regarding the error.
+        },
+    });
 
-  const submit = (request: RegisterUserRequest) => {
-    setErrorMessages([]);
+    const submit = (request: RegisterUserRequest) => {
+        setErrorMessages([]);
 
-    registerMutation(request);
-  };
+        registerMutation(request);
+    };
 
-  return {
-    submit,
-    errorMessages,
-    isPending,
-    isSuccess
-  };
+    return {
+        submit,
+        errorMessages,
+        isPending,
+        isSuccess,
+    };
 };
