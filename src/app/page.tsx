b1f753db43df482f426e1f22ecf6b59f28cc3e2c
Amir <haghighi251@gmail.com>
2024-05-25 14:54:52 +0200

    refactor: update and remove the language changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,13 @@ import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 
 import { LanguageSwitcher } from '@/types/components/LanguageSwitcher/LanguageSwitcher';
-import { getServerTranslations } from '@/types/utils/i18n/server';
 
 export default function Page() {
     const { t } = useTranslation();
     return (
         <>
             <h1>
-                {t('name')}
-                {t('title')}
+                {t('home_page_title')}
             </h1>
             <Link href={`/second-page`}>{t('hello')}</Link>
             <LanguageSwitcher />
