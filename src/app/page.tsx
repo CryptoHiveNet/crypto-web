0811464c65241a558237091ffb7b408557b39e28
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-05-25 17:01:50 +0330

    refactor: changed the language switcher path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,15 @@
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 
-import { LanguageSwitcher } from '@/types/components/LanguageSwitcher/LanguageSwitcher';
+import { LanguageSwitcher } from '@/types/shared/components/LanguageSwitcher/LanguageSwitcher';
 
 export default function Page() {
     const { t } = useTranslation();
     return (
         <>
-            <h1>
-                {t('home_page_title')}
-            </h1>
-            <Link href={`/second-page`}>{t('hello')}</Link>
+            <h1>{t('home_page_title')}</h1>
+            <Link href={`/second-page`}>{t('second-page')}</Link>
             <LanguageSwitcher />
-            <div>Testing</div>
         </>
     );
 }
